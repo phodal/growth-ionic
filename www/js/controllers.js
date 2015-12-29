55eb73326475a9e1a9062fd6e90a2c9c4fa50b85
Fengda HUANG <h@phodal.com>
2015-12-29 14:47:10 +0800

    Add modal

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -8,8 +8,23 @@ angular.module('starter.controllers', [])
 
   })
 
-  .controller('DayCtrl1', function ($scope) {
+  .controller('DayCtrl1', function ($scope, $ionicModal) {
+    $ionicModal.fromTemplateUrl('templates/modal/tree.html', {
+      scope: $scope
+    }).then(function (modal) {
+      $scope.modal = modal;
+    });
 
+    $scope.contacts = [
+      {name: 'Gordon Freeman'},
+      {name: 'Barney Calhoun'},
+      {name: 'Lamarr the Headcrab'}
+    ];
+
+    $scope.createContact = function (u) {
+      $scope.contacts.push({name: u.firstName + ' ' + u.lastName});
+      $scope.modal.hide();
+    };
   })
 
   .controller('SearchCtrl', function ($scope) {
