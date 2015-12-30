77d21d37a00ef5275803f4603bfa3f97c46ebcc3
Fengda HUANG <h@phodal.com>
2015-12-30 21:01:14 +0800

    add mock & tdd for day2

--- www/article/articles.js
+++ www/article/articles.js
@@ -10,5 +10,13 @@ var AllArticle = [
   {
     "slug" : "javascript",
     "title": "无处不在的JavaScript"
+  },
+  {
+    "slug" : "mock",
+    "title": "Mock和Stub"
+  },
+  {
+    "slug" : "tdd",
+    "title": "测试驱动开发"
   }
 ];
