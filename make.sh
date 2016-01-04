a473c76ecf4e7c2b9dcba9bdd694dac1674fef75
Fengda HUANG <h@phodal.com>
2016-01-04 17:55:16 +0800

    remove crosswalk & release new version

--- make.sh
+++ make.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cordova build --release android
 rm ~/repractise/growth/platforms/android/build/outputs/apk/growth.apk
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/phodal.keystore ~/repractise/growth/platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk phodal
-~/android-sdk/build-tools/22.0.0/zipalign -v 4 platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk ~/repractise/growth/platforms/android/build/outputs/apk/growth.apk
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/phodal.keystore ~/repractise/growth/platforms/android/build/outputs/apk/android-release-unsigned.apk phodal
+~/android-sdk/build-tools/22.0.0/zipalign -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk ~/repractise/growth/platforms/android/build/outputs/apk/growth.apk
