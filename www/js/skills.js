a809305afe611fd94f31bb88132b0ecdef84e22d
Fengda HUANG <h@phodal.com>
2016-01-05 22:13:28 +0800

    add more get skills

--- www/js/skills.js
+++ www/js/skills.js
@@ -2,12 +2,37 @@ var AllSkills = {
   "hello": [
     {text: "HTML5"},
     {text: "CSS3"},
+    {text: "Node.js"},
+    {text: "Java"},
+    {text: "Python"},
     {text: "JavaScript"}
   ],
   "env": [
-    {text: "OS X"}
+    {text: "Windows"},
+    {text: "Chrome"},
+    {text: "IE / Edge"},
+    {text: "Firefox"},
+    {text: "GNU/Linux"}
   ],
   "build": [
-    {text: "Code"}
+    {text: "Npm"},
+    {text: "Grunt"},
+    {text: "Make"},
+    {text: "Gradle"}
+  ],
+  "before": [
+    {text: "Tasking"},
+    {text: "KickOff"}
+  ],
+  "test": [
+    {text: "Unit Test"},
+    {text: "Functional Test"},
+    {text: "Integration Test"},
+    {text: "TDD"}
+  ],
+  "coding": [
+    {text: "Rename"},
+    {text: "Extract Variable"},
+    {text: "Inline"}
   ]
-}
+};
