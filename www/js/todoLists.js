dde56efef9382063d907f8318548e0f034bf9a39
Fengda HUANG <h@phodal.com>
2016-01-11 09:46:24 +0800

    add more todo lists

--- www/js/todoLists.js
+++ www/js/todoLists.js
@@ -21,6 +21,22 @@ var TODO_LISTS = {
       {
         "id": "try-angular",
         "title": "尝试使用angular"
+      },
+      {
+        "id": "try-backbone",
+        "title": "尝试使用Backbone"
+      },
+      {
+        "id": "try-backbone-react",
+        "title": "结合Backbone和React"
+      },
+      {
+        "id": "try-react-webpack",
+        "title": "结合React和Webpack"
+      },
+      {
+        "id": "try-browserify",
+        "title": "使用browserify和npm"
       }
     ],
     "middle": []
@@ -30,6 +46,18 @@ var TODO_LISTS = {
       {
         "id": "try-express",
         "title": "尝试使用express"
+      },
+      {
+        "id": "try-spring",
+        "title": "尝试使用Spring MVC"
+      },
+      {
+        "id": "try-mysql-sqlite",
+        "title": "试用不同的数据库MySQL和SQLite"
+      },
+      {
+        "id": "try-mongodb",
+        "title": "试用不同的NoSQL数据库——MongoDB"
       }
     ],
     "middle": []
@@ -51,6 +79,27 @@ var TODO_LISTS = {
       {
         "id": "try-keyshorts",
         "title": "尝试使用快捷键代替鼠标操作"
+      }
+    ],
+    "middle": []
+  },
+  "test": {
+    "basic": [
+      {
+        "id": "try-test",
+        "title": "尝试写一个简单的测试"
+      },
+      {
+        "id": "try-mock",
+        "title": "尝试写一个Mock测试"
+      },
+      {
+        "id": "try-stub",
+        "title": "尝试写一个Stub测试"
+      },
+      {
+        "id": "try-tdd",
+        "title": "尝试TDD"
       }
     ],
     "middle": []
