b04a3742fff2a972d1d9d307a453bfd33cb1bc78
Fengda HUANG <h@phodal.com>
2016-01-12 16:28:06 +0800

    add version for update page

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,14 @@
 angular.module('starter.controllers', ['starter.factory', 'starter.services', 'hljs', 'starter.utils'])
-  .controller('AboutCtrl', function ($scope, $analytics) {
+  .controller('AboutCtrl', function ($scope, $analytics, $updateServices, $cordovaAppVersion) {
     $analytics.trackView('About Ctrl');
     $scope.isApp = window.cordova !== undefined;
+    $scope.version = '0.0.0';
+    $cordovaAppVersion.getVersionNumber().then(function (version) {
+      $scope.version = version;
+    });
+    $scope.update = function () {
+      $updateServices.check();
+    }
   })
 
   .controller('SolutionCtrl', function ($scope, $analytics) {
