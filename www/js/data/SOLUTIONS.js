256aeecddfd7a5516e2ef676f74792a940fb2713
Fengda HUANG <h@phodal.com>
2016-01-12 20:44:42 +0800

    Add solution desc

--- www/js/data/SOLUTIONS.js
+++ www/js/data/SOLUTIONS.js
@@ -1,26 +1,26 @@
 var SOLUTIONS = [{
-  "name": "Nginx/Apache Log Analytics",
+  "name": "Nginx/Apache日志分析",
   "type": "server",
-  "stacks": ["Hadoop", "Pig", "Jython", "amMap", "ElasticSearch"],
-  "description": ""
+  "stacks": ["Hadoop", "amMap", "ElasticSearch"],
+  "description": "用Hadoop分析、解析数据，将数据存储到ElasticSearch中，通过API与amMap再显示到地图上。"
 }, {
-  "name": "Mobile Application",
+  "name": "移动应用",
   "type": "mobile",
-  "stacks": ["Ionic", "Cordova", "ngCordova", "Angular"],
-  "description": ""
+  "stacks": ["Ionic", "Cordova", "ngCordova"],
+  "description": "Ionic封装了Cordova和Angular，并辅助以ngCordova来封装一些Cordova插件。"
 }, {
-  "name": "Web Application",
+  "name": "Web应用",
   "type": "generic",
   "stacks": ["Gulp", "React", "WebPack", "Babel", "Express"],
-  "description": ""
+  "description": "Babel用于转换React中的ES6的语法，再中WebPack打包js。"
 }, {
-  "name": "Web Application",
+  "name": "Web应用",
   "type": "server",
   "stacks": ["Flask", "Jinja2", "SQLAlchemy"],
-  "description": ""
+  "description": "Jinja2作为模板引擎，SQLAlchemy作为ORM，Flask就可以起飞了~~。"
 }, {
-  "name": "Web Application",
+  "name": "Web应用",
   "type": "server",
   "stacks": ["Spring MVC", "iBATIS", "Gradle"],
-  "description": ""
+  "description": "Java Web的经典。"
 }];
