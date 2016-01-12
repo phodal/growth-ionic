078a72e16e4ae59956a4ab9ce6b46f07247bd272
Fengda HUANG <h@phodal.com>
2016-01-12 16:00:12 +0800

    Extract to files

---
+++ www/js/SOLUTIONS.js
@@ -0,0 +1,26 @@
+var SOLUTIONS = [{
+  "name": "Nginx/Apache Log Analytics",
+  "type": "server",
+  "stacks": ["Hadoop", "Pig", "Jython", "amMap", "ElasticSearch"],
+  "description": ""
+}, {
+  "name": "Mobile Application",
+  "type": "mobile",
+  "stacks": ["Ionic", "Cordova", "ngCordova", "Angular"],
+  "description": ""
+}, {
+  "name": "Web Application",
+  "type": "generic",
+  "stacks": ["Gulp", "React", "WebPack", "Babel", "Express"],
+  "description": ""
+}, {
+  "name": "Web Application",
+  "type": "server",
+  "stacks": ["Flask", "Jinja2", "SQLAlchemy"],
+  "description": ""
+}, {
+  "name": "Web Application",
+  "type": "server",
+  "stacks": ["Spring MVC", "iBATIS", "Gradle"],
+  "description": ""
+}];
