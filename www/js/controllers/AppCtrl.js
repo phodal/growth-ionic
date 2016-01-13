46be7913b94593e5b48d20e5ace1420068fe835d
Fengda HUANG <h@phodal.com>
2016-01-13 11:26:34 +0800

    optimize for first page

--- www/js/controllers/AppCtrl.js
+++ www/js/controllers/AppCtrl.js
@@ -1,9 +1,8 @@
 angular.module('app.AppControl', ['starter.factory', 'hljs', 'starter.utils'])
   .controller('AppCtrl', function ($scope, $storageServices, $updateServices) {
-
-    $updateServices.check('main');
     $scope.badgePoints = 0;
-    $scope.$on('$ionicView.enter', function () {
+    $scope.$on('$ionicView.afterEnter', function () {
+      $updateServices.check('main');
       $storageServices.get('badgePoints', function (points) {
         $scope.badgePoints = points;
       });
