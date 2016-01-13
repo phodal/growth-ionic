0fc91cce501b88b2fa13f7b66cfa94651907e4e1
Fengda HUANG <h@phodal.com>
2016-01-13 20:49:12 +0800

    add mean solution

--- www/js/data/SOLUTIONS.js
+++ www/js/data/SOLUTIONS.js
@@ -4,27 +4,27 @@ var SOLUTIONS = [{
   "stacks": ["Hadoop", "amMap", "ElasticSearch"],
   "description": "用Hadoop分析、解析数据，将数据存储到ElasticSearch中，通过API与amMap再显示到地图上。"
 }, {
-  "name": "移动应用",
+  "name": "移动混合应用",
   "type": "mobile",
   "stacks": ["Ionic", "Cordova", "ngCordova"],
   "description": "Ionic封装了Cordova和Angular，并辅助以ngCordova来封装一些Cordova插件。"
 }, {
-  "name": "Web应用",
+  "name": "Node.js React Web应用",
   "type": "generic",
-  "stacks": ["Gulp", "React", "WebPack", "Babel", "Express"],
+  "stacks": ["React", "WebPack", "Babel", "Express"],
   "description": "Babel用于转换React中的ES6的语法，再中WebPack打包js。"
 }, {
-  "name": "Web应用",
+  "name": "Python Flask Web应用",
   "type": "server",
   "stacks": ["Flask", "Jinja2", "SQLAlchemy"],
   "description": "Jinja2作为模板引擎，SQLAlchemy作为ORM，Flask就可以起飞了~~。"
 }, {
-  "name": "Web应用",
+  "name": "Java Web应用",
   "type": "server",
   "stacks": ["Spring MVC", "iBATIS", "Gradle"],
   "description": "Java Web的经典。"
 }, {
-  "name": "Web应用",
+  "name": "Python Web应用",
   "type": "server",
   "stacks": ["Django", "Django JWT", "Django DRF"],
   "description": "单页面应用后台。"
@@ -33,4 +33,14 @@ var SOLUTIONS = [{
   "type": "server",
   "stacks": ["Lucene", "Java"],
   "description": "全文搜索解决方案。"
+}, {
+  "name": "Web应用 - MEAN",
+  "type": "generic",
+  "stacks": ["MongoDB", "Express", "Angular.js", "Node.js"],
+  "description": "Node.js的Web解决方案——MEAN。"
+}, {
+  "name": "Scala Web应用",
+  "type": "generic",
+  "stacks": ["Scala", "Play"],
+  "description": "Play是Scala的Web框架。"
 }];
