55eab9dd2b32eaabc479e0809d86b530e88040b1
Fengda HUANG <h@phodal.com>
2016-01-14 12:28:04 +0800

    Add some achieventment

--- www/js/controllers/AchievementCtrl.js
+++ www/js/controllers/AchievementCtrl.js
@@ -1,5 +1,18 @@
 angular.module('app.AchievementCtrl', ['starter.factory', 'hljs', 'starter.utils'])
   .controller('AchievementCtrl', function ($scope, $ionicModal, $storageServices, $analytics) {
     $analytics.trackView('Achievement Controller');
+    $scope.achievements = [];
+    $scope.todoMenus = TODO_LISTS;
+    var todoMenuKeys = Object.keys(TODO_LISTS);
 
+    angular.forEach(todoMenuKeys, function (listsKey) {
+      $storageServices.get(listsKey + 'Finish', function (result) {
+        if (result === 'true') {
+          $scope.todoMenus[listsKey]['isFinish'] = true;
+          if($scope.todoMenus[listsKey].achievement !== undefined){
+            $scope.achievements.push($scope.todoMenus[listsKey].achievement);
+          }
+        }
+      })
+    });
   });
--- www/js/data/TODO_LISTS.js
+++ www/js/data/TODO_LISTS.js
@@ -1,194 +1,204 @@
 var TODO_LISTS = {
-  "hello": {
-    "zh": "入门",
-    "basic": [
+  'hello': {
+    'zh': '入门',
+    'achievement': 'Hello, World',
+    'basic': [
       {
-        "id": "try-helloworld",
-        "title": "用这个语言写hello,world"
+        'id': 'try-helloworld',
+        'title': '用这个语言写hello,world'
       },
       {
-        "id": "learn-grammar",
-        "title": "学习一门语言的语法"
+        'id': 'learn-grammar',
+        'title': '学习一门语言的语法'
       },
       {
-        "id": "do-todo-list",
-        "title": "实现一个TODO List"
+        'id': 'do-todo-list',
+        'title': '实现一个TODO List'
       }
     ],
-    "middle": []
+    'middle': []
   },
-  "before": {
+  'before': {
     'zh': '编码前',
-    "basic": [
+    'achievement': '一点就通',
+    'basic': [
       {
-        "id": "try-launch",
-        "title": "在你所使用的操作系统上使用启动器来运行软件。"
+        'id': 'try-launch',
+        'title': '在你所使用的操作系统上使用启动器来运行软件。'
       },
       {
-        "id": "remember-keyshort",
-        "title": "熟记你所使用的IDE、编辑器的各种快捷键。"
+        'id': 'remember-keyshort',
+        'title': '熟记你所使用的IDE、编辑器的各种快捷键。'
       },
       {
-        "id": "use-a-auto-script",
-        "title": "使用自动化脚本来完成一些简单的工作，如打开邮件。"
+        'id': 'use-a-auto-script',
+        'title': '使用自动化脚本来完成一些简单的工作，如打开邮件。'
       }
     ],
-    "middle": []
+    'middle': []
   },
-  "front": {
+  'front': {
     'zh': '前端',
-    "basic": [
+    'achievement': '前端',
+    'basic': [
       {
-        "id": "try-angular",
-        "title": "尝试使用angular"
+        'id': 'try-angular',
+        'title': '尝试使用angular'
       },
       {
-        "id": "try-backbone",
-        "title": "尝试使用Backbone"
+        'id': 'try-backbone',
+        'title': '尝试使用Backbone'
       },
       {
-        "id": "try-backbone-react",
-        "title": "结合Backbone和React"
+        'id': 'try-backbone-react',
+        'title': '结合Backbone和React'
       },
       {
-        "id": "try-react-webpack",
-        "title": "结合React和Webpack"
+        'id': 'try-react-webpack',
+        'title': '结合React和Webpack'
       },
       {
-        "id": "try-browserify",
-        "title": "使用browserify和npm"
+        'id': 'try-browserify',
+        'title': '使用browserify和npm'
       }
     ],
-    "middle": []
+    'middle': []
   },
-  "mvc": {
+  'mvc': {
     'zh': '后台',
-    "basic": [
+    'achievement': '后台',
+    'basic': [
       {
-        "id": "try-express",
-        "title": "尝试使用express"
+        'id': 'try-express',
+        'title': '尝试使用express'
       },
       {
-        "id": "try-spring",
-        "title": "尝试使用Spring MVC"
+        'id': 'try-spring',
+        'title': '尝试使用Spring MVC'
       },
       {
-        "id": "try-mysql-sqlite",
-        "title": "试用不同的数据库MySQL和SQLite"
+        'id': 'try-mysql-sqlite',
+        'title': '试用不同的数据库MySQL和SQLite'
       },
       {
-        "id": "try-mongodb",
-        "title": "试用不同的NoSQL数据库——MongoDB"
+        'id': 'try-mongodb',
+        'title': '试用不同的NoSQL数据库——MongoDB'
       }
     ],
-    "middle": []
+    'middle': []
   },
-  "env": {
+  'env': {
     'zh': '环境',
-    "basic": [
+    'achievement': '环境',
+    'basic': [
       {
-        "id": "try-linux",
-        "title": "尝试使用GNU/Linux"
+        'id': 'try-linux',
+        'title': '尝试使用GNU/Linux'
       },
       {
-        "id": "try-editor-ide",
-        "title": "尝试使用不同的IDE和编辑器"
+        'id': 'try-editor-ide',
+        'title': '尝试使用不同的IDE和编辑器'
       },
       {
-        "id": "try-cli",
-        "title": "尝试使用命令行工具"
+        'id': 'try-cli',
+        'title': '尝试使用命令行工具'
       },
       {
-        "id": "try-keyshorts",
-        "title": "尝试使用快捷键代替鼠标操作"
+        'id': 'try-keyshorts',
+        'title': '尝试使用快捷键代替鼠标操作'
       }
     ],
-    "middle": []
+    'middle': []
   },
-  "test": {
+  'test': {
     'zh': '测试',
-    "basic": [
+    'achievement': '测试',
+    'basic': [
       {
-        "id": "try-test",
-        "title": "尝试写一个简单的测试"
+        'id': 'try-test',
+        'title': '尝试写一个简单的测试'
       },
       {
-        "id": "try-mock",
-        "title": "尝试写一个Mock测试"
+        'id': 'try-mock',
+        'title': '尝试写一个Mock测试'
       },
       {
-        "id": "try-stub",
-        "title": "尝试写一个Stub测试"
+        'id': 'try-stub',
+        'title': '尝试写一个Stub测试'
       },
       {
-        "id": "try-tdd",
-        "title": "尝试TDD"
+        'id': 'try-tdd',
+        'title': '尝试TDD'
       }
     ],
-    "middle": []
+    'middle': []
   },
-  "refactor": {
+  'refactor': {
     'zh': '重构',
-    "basic": [
+    'achievement': '魔法师',
+    'basic': [
       {
-        "id": "try-refactor-var",
-        "title": "尝试做一些简单的重命名变量"
+        'id': 'try-refactor-var',
+        'title': '尝试做一些简单的重命名变量'
       },
       {
-        "id": "try-refactor-func",
-        "title": "尝试做一些重命名现有的函数"
+        'id': 'try-refactor-func',
+        'title': '尝试做一些重命名现有的函数'
       }
     ],
-    "middle": []
+    'middle': []
   },
-  "configurable": {
+  'configurable': {
     'zh': '可配置',
-    "basic": [
+    'achievement': '会点魔术',
+    'basic': [
       {
-        "id": "try-configurable",
-        "title": "使用一些针对不同开发环境的插件"
+        'id': 'try-configurable',
+        'title': '使用一些针对不同开发环境的插件'
       },
       {
-        "id": "try-toggle",
-        "title": "用Toggle来管理应用的一些新特性"
+        'id': 'try-toggle',
+        'title': '用Toggle来管理应用的一些新特性'
       }
     ],
-    "middle": []
+    'middle': []
   },
-  "ci": {
+  'ci': {
     'zh': '持续集成',
-    "basic": [
+    'achievement': '构建师',
+    'basic': [
       {
-        "id": "try-ci",
-        "title": "尝试搭建CI环境"
+        'id': 'try-ci',
+        'title': '尝试搭建CI环境'
       },
       {
-        "id": "try-build",
-        "title": "尝试使用CI创建Build"
+        'id': 'try-build',
+        'title': '尝试使用CI创建Build'
       }
     ],
-    "middle": []
+    'middle': []
   },
-  "container": {
+  'container': {
     'zh': '应用容器',
-    "basic": [
+    'achievement': '建筑师',
+    'basic': [
       {
-        "id": "install-linux-in-vm",
-        "title": "使用虚拟机安装GNU/Linux操作系统"
+        'id': 'install-linux-in-vm',
+        'title': '使用虚拟机安装GNU/Linux操作系统'
       },
       {
-        "id": "understand-jetty-tomcat",
-        "title": "理解Tomcat和Jetty的关系"
+        'id': 'understand-jetty-tomcat',
+        'title': '理解Tomcat和Jetty的关系'
       },
       {
-        "id": "use-docker-install-app",
-        "title": "使用Docker安装应用程序"
+        'id': 'use-docker-install-app',
+        'title': '使用Docker安装应用程序'
       },
       {
-        "id": "use-docker-as-container",
-        "title": "使用Docker作应用容器"
+        'id': 'use-docker-as-container',
+        'title': '使用Docker作应用容器'
       }
     ],
-    "middle": []
+    'middle': []
   }
 };
