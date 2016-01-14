044d48a168c28e729d58c05f49b1488b479d178a
Fengda HUANG <h@phodal.com>
2016-01-14 12:13:02 +0800

    update badge logic

--- www/js/controllers/AppCtrl.js
+++ www/js/controllers/AppCtrl.js
@@ -1,7 +1,3 @@
 angular.module('app.AppControl', ['starter.factory', 'hljs', 'starter.utils'])
-  .controller('AppCtrl', function ($scope, $storageServices) {
-    $scope.badgePoints = 0;
-    $storageServices.get('badgePoints', function (points) {
-      $scope.badgePoints = points;
-    });
+  .controller('AppCtrl', function ($scope) {
   });
--- www/js/controllers/MainCtrl.js
+++ www/js/controllers/MainCtrl.js
@@ -2,6 +2,11 @@ angular.module('app.MainCtrl', ['starter.factory', 'hljs', 'starter.utils'])
   .controller('MainCtrl', function ($scope, $ionicModal, $storageServices, $analytics, $ionicPopover, $updateServices) {
     $updateServices.check('main');
     $scope.$on('$ionicView.beforeEnter', function () {
+      $scope.badgePoints = 0;
+      $storageServices.get('badgePoints', function (points) {
+        $scope.badgePoints = points;
+      });
+      
       $storageServices.get('isFirstTime', function (value) {
         if (value !== 'false') {
           $ionicModal.fromTemplateUrl('templates/intro/intro.html', {
