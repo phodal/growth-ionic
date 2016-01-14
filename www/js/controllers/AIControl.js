8145b2ece38c658668150521d9619a98f2b74d98
Fengda HUANG <h@phodal.com>
2016-01-14 12:40:31 +0800

    add simple ai

--- www/js/controllers/AIControl.js
+++ www/js/controllers/AIControl.js
@@ -1,5 +1,18 @@
 angular.module('app.AIControl', ['starter.factory', 'hljs', 'starter.utils'])
   .controller('AIControl', function ($scope, $ionicModal, $storageServices, $analytics) {
     $analytics.trackView('AI Controller');
+    $analytics.trackView('Achievement Controller');
+    $scope.improves = [];
+    $scope.todoMenus = TODO_LISTS;
+    var todoMenuKeys = Object.keys(TODO_LISTS);
+
+    angular.forEach(todoMenuKeys, function (listsKey) {
+      $storageServices.get(listsKey + 'Finish', function (result) {
+        if (result !== 'true') {
+          $scope.todoMenus[listsKey]['isFinish'] = true;
+          $scope.improves.push($scope.todoMenus[listsKey]);
+        }
+      })
+    });
 
   });
