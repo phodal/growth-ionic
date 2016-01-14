96ee3df07dc612fd52257d59a1e32a16a2345cf6
Fengda HUANG <h@phodal.com>
2016-01-14 13:21:30 +0800

    add series books

--- www/review/BOOK_REVIEWS.js
+++ www/review/BOOK_REVIEWS.js
@@ -1,35 +1,55 @@
 var BOOK_REVIEWS = [
   {
-    "slug" : "python-tdd",
+    "slug": "python-tdd",
     "title": "《Python Web开发 - 测试驱动方法》",
     "store": "http://www.amazon.cn/Python-Web%E5%BC%80%E5%8F%91-%E6%B5%8B%E8%AF%95%E9%A9%B1%E5%8A%A8%E6%96%B9%E6%B3%95-%E7%8F%80%E8%A5%BF%E7%93%A6%E5%B0%94/dp/B016I9T8SQ/ref=sr_1_1?ie=UTF8&qid=1452691421&sr=8-1&keywords=Python+Web%E5%BC%80%E5%8F%91+-+%E6%B5%8B%E8%AF%95%E9%A9%B1%E5%8A%A8%E6%96%B9%E6%B3%95"
   },
   {
-    "slug" : "sicp",
+    "slug": "sicp",
     "title": "《计算机程序的构造和解释》"
   },
   {
-    "slug" : "head-first",
+    "slug": "head-first",
     "title": "《Head First》"
   },
   {
-    "slug" : "thinking-in-java",
+    "slug": "thinking-in-java",
     "title": "《Java编程思想》",
     "store": "http://www.amazon.cn/Java%E7%BC%96%E7%A8%8B%E6%80%9D%E6%83%B3-%E5%9F%83%E5%8F%B2%E5%B0%94/dp/B0011F7WU4"
   },
   {
-    "slug" : "tdd",
+    "slug": "tdd",
     "title": "《测试驱动开发》",
     "store": "http://book.douban.com/subject/1230036/"
   },
   {
-    "slug" : "productive-programmer",
+    "slug": "productive-programmer",
     "title": "《卓有成效的程序员》",
     "store": "http://www.amazon.cn/%E5%8D%93%E6%9C%89%E6%88%90%E6%95%88%E7%9A%84%E7%A8%8B%E5%BA%8F%E5%91%98-%E5%BC%97%E5%BE%B7/dp/B001XCWFOI/ref=sr_1_1?s=books&ie=UTF8&qid=1452699898&sr=1-1&keywords=%E5%8D%93%E6%9C%89%E6%88%90%E6%95%88%E7%9A%84%E7%A8%8B%E5%BA%8F%E5%91%98"
   },
   {
-    "slug" : "tdd-art",
+    "slug": "tdd-art",
     "title": "《测试驱动开发的艺术》",
     "store": "http://book.douban.com/subject/5326182/"
+  },
+  {
+    "slug": "head-first-dp",
+    "title": "《Head First 设计模式》",
+    "store": "http://www.amazon.cn/Head-First%E8%AE%BE%E8%AE%A1%E6%A8%A1%E5%BC%8F-%E5%BC%97%E9%87%8C%E6%9B%BC/dp/B0011FBU34"
+  },
+  {
+    "slug": "dp",
+    "title": "《设计模式》",
+    "store": "http://www.amazon.cn/%E8%AE%A1%E7%AE%97%E6%9C%BA%E7%A7%91%E5%AD%A6%E4%B8%9B%E4%B9%A6-%E8%AE%BE%E8%AE%A1%E6%A8%A1%E5%BC%8F-%E5%8F%AF%E5%A4%8D%E7%94%A8%E9%9D%A2%E5%90%91%E5%AF%B9%E8%B1%A1%E8%BD%AF%E4%BB%B6%E7%9A%84%E5%9F%BA%E7%A1%80-Erich-Gamma/dp/B001130JN8/ref=pd_sim_14_4?ie=UTF8&dpID=51EMEzFDfXL&dpSrc=sims&preST=_AC_UL160_SR101%2C160_&refRID=02X18TFPV1GP0NZYD670"
+  },
+  {
+    "slug": "refactor-to-pattern",
+    "title": "《重构与模式》",
+    "store": "http://book.douban.com/subject/1052241/"
+  },
+  {
+    "slug": "elemental-dp",
+    "title": "《元素模式》",
+    "store": "http://book.douban.com/subject/25908396/"
   }
 ];
