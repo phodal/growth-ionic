46795466c0dcbcbefd2e46fc6e4318bc248c3776
Fengda HUANG <h@phodal.com>
2016-01-14 21:45:09 +0800

    add nools hello world

--- www/js/controllers/AIControl.js
+++ www/js/controllers/AIControl.js
@@ -1,6 +1,31 @@
 angular.module('app.AIControl', ['starter.factory', 'hljs', 'starter.utils'])
-  .controller('AIControl', function ($scope, $ionicModal, $storageServices, $analytics) {
+  .controller('AIControl', function ($scope, $ionicModal, $storageServices, $analytics, $http) {
     $analytics.trackView('AI Controller');
+
+    $http.get('/rules/test.nools').then(function(response){
+      var logger = function(res){
+        console.log(res);
+      };
+      var flow = nools.compile(response.data, {
+          scope: {
+            logger: logger
+          },
+          name: 'AI Flow'
+        });
+      var session = flow.getSession();
+      var Message = flow.getDefined("message");
+
+      session.assert(new Message("goodbye"));
+      session.assert(new Message("hello"));
+      session.assert(new Message("hello world"));
+      session.match(function(err){
+        if(err){
+          console.error(err.stack);
+        }else{
+          console.log("done");
+        }
+      })
+    });
     $scope.improves = [];
     $scope.aiTodoLists = TODO_LISTS;
     var todoMenuKeys = Object.keys(TODO_LISTS);
