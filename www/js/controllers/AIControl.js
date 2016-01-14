3cd99fd56908192c221f2fda0e439e2d22888411
Fengda HUANG <h@phodal.com>
2016-01-14 22:42:02 +0800

    Add ai hello world

--- www/js/controllers/AIControl.js
+++ www/js/controllers/AIControl.js
@@ -2,29 +2,38 @@ angular.module('app.AIControl', ['starter.factory', 'hljs', 'starter.utils'])
   .controller('AIControl', function ($scope, $ionicModal, $storageServices, $analytics, $http) {
     $analytics.trackView('AI Controller');
 
-    $http.get('/rules/test.nools').then(function(response){
-      var logger = function(res){
+    $http.get('/rules/test.nools').then(function (response) {
+      var logger = function (res) {
         console.log(res);
       };
       var flow = nools.compile(response.data, {
-          scope: {
-            logger: logger
-          },
-          name: 'AI Flow'
-        });
-      var session = flow.getSession();
-      var Message = flow.getDefined("message");
+        scope: {
+          logger: logger
+        },
+        name: 'AI Flow'
+      });
+      var session = flow.getSession().on("fire", function (ruleName) {
+        //console.log(ruleName);
+      });
+      var SkillCal = flow.getDefined("skillcal");
 
-      session.assert(new Message("goodbye"));
-      session.assert(new Message("hello"));
-      session.assert(new Message("hello world"));
-      session.match(function(err){
-        if(err){
+      session.assert(new SkillCal("server", 5));
+      session.match(function (err) {
+        if (err) {
           console.error(err.stack);
-        }else{
+        } else {
           console.log("done");
         }
-      })
+      });
+      var finallyWords = "";
+      var firstWord = "你是一个";
+
+      finallyWords = finallyWords + firstWord;
+      angular.forEach(session.getFacts(), function(fact){
+        finallyWords = finallyWords + fact.text;
+      });
+
+      $scope.finallyWords = finallyWords;
     });
     $scope.improves = [];
     $scope.aiTodoLists = TODO_LISTS;
