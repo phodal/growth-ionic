670e3a170a77474b9d597f9eced034bd34478ea7
Fengda HUANG <h@phodal.com>
2016-01-15 15:54:54 +0800

    add day menus

--- www/i18n/menu.js
+++ www/i18n/menu.js
@@ -5,7 +5,14 @@ var MENU = {
     solution: "Solution",
     feedback: "Feedback",
     help: "Help",
-    about: "About"
+    about: "About",
+    "day1": "Start",
+    "day2": "Coding",
+    "day3": "Go Live",
+    "day4": "Analytics",
+    "day5": "CI",
+    "day6": "Legacy System",
+    "day7": "Retro & Restart"
   },
   "zh-cn": {
     exam: "技能测验",
@@ -13,6 +20,13 @@ var MENU = {
     solution: "解决方案(Beta)",
     feedback: "意见和反馈",
     help: "使用指南",
-    about: "关于"
+    about: "关于",
+    "day1": "从零开始",
+    "day2": "编码",
+    "day3": "上线",
+    "day4": "数据分析",
+    "day5": "持续交付",
+    "day6": "遗留系统",
+    "day7": "回顾与新架构"
   }
 };
