96a4552f5d31275a18297893fd9a97e9600f4be7
Le Deng <ray.deng83@gmail.com>
2016-01-17 21:28:22 +0000

    added EN translation for Exam, Book RoadMap, About

--- www/i18n/en.js
+++ www/i18n/en.js
@@ -1,21 +1,22 @@
 SIDE_MENU["en"] = {
-  exam: "Exam",
-  bookroadmap: "Book RoadMap",
-  solution: "Solution",
-  feedback: "Feedback",
-  help: "Help",
-  about: "About"
+  "exam": "Exam",
+  "bookroadmap": "Book RoadMap",
+  "solution": "Solution",
+  "feedback": "Feedback",
+  "help": "Help",
+  "about": "About",
+  "title" : "Become A Cool Developer"
 };
 
 MAIN_VIEW_MENU["en"] = {
-  "day0": "Prepare",
-  "day1": "Start",
-  "day2": "Coding",
-  "day3": "Go Live",
-  "day4": "Analytics",
-  "day5": "CI",
-  "day6": "Legacy System",
-  "day7": "Retro & Restart"
+  "day0": "Day 0 : Prepare",
+  "day1": "Day 1 : Start",
+  "day2": "Day 2 : Coding",
+  "day3": "Day 3 : Go Live",
+  "day4": "Day 4 : Analytics",
+  "day5": "Day 5 : CI",
+  "day6": "Day 6 : Legacy System",
+  "day7": "Day 7 : Retro & Restart"
 };
 
 PAGE_TITLE["en"] = {
@@ -36,4 +37,31 @@ DAY_TITLE["en"] = {
   "container": "Container",
   "configurable": "Configurable",
   "books": "Books"
+};
+
+ABOUT_MENU["en"] = {
+  "update" : "Check for update",
+  "current-version" : "Current version",
+  "git-hub" : "Git-Hub",
+  "qq-group" : "QQ Group"
+};
+
+EXAM_MENU["en"] = {
+  "programming-language-game" : "Programming Language (Questions)",
+  "html" : "HTML",
+  "css" : "CSS",
+  "javascript" : "JavaScript",
+  "front-end" : "Front-End",
+  "node-js" : "Node.js",
+  "refactor" : "Refactor"
+};
+
+BOOK_ROADMAP_MENU["en"] = {
+  "thoughtworks-reading-roadmap" : "Reading-RoadMap",
+  "programming-practice" : "Practical Programming",
+  "architecture" : "Architecture and Frame",
+  "methodology" : "methodology",
+  "leadership" : "Thinking and Leading",
+  "specific-domain" : "Specific-Focus-Area Reading-RoadMap",
+  "front-end" : "Front-End"
 };
--- www/i18n/main.js
+++ www/i18n/main.js
@@ -5,3 +5,9 @@ var MAIN_VIEW_MENU = {};
 var PAGE_TITLE = {};
 
 var DAY_TITLE = {};
+
+var ABOUT_MENU={};
+
+var EXAM_MENU={};
+
+var BOOK_ROADMAP_MENU={};
