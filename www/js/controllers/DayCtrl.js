f05ebfe4080167a3117de94c048da2c8172fd2c8
Fengda HUANG <h@phodal.com>
2016-01-20 11:06:05 +0800

    add more files

--- www/js/controllers/DayCtrl.js
+++ www/js/controllers/DayCtrl.js
@@ -9,44 +9,6 @@ angular.module('app.dayController', ['starter.factory', 'hljs', 'starter.utils']
     angular.forEach(Object.keys(TODO_LISTS), function (key) {
       $scope.allDoneItems[key] = [];
     });
-    var HELPER_ARTICLES = {
-      hello: [
-        {
-          slug: 'how-to-learn',
-          title: '如何学好一门技术、语言'
-        },
-        {
-          slug: 'html',
-          title: '无处不在的HTML'
-        },
-        {
-          slug: 'css',
-          title: '无处不在的CSS'
-        },
-        {
-          slug: 'javascript',
-          title: '无处不在的JavaScript'
-        }
-      ],
-      env: [
-        {
-          slug: 'windows-unix',
-          title: 'Windows或者类Unix'
-        },
-        {
-          slug: 'pm',
-          title: '包管理'
-        },
-        {
-          slug: 'env-windows',
-          title: '搭建开发环境(Windows)'
-        },
-        {
-          slug: 'env-osx',
-          title: '搭建开发环境(OSX)'
-        }
-      ]
-    };
 
     $scope.HELPER = HELPER_ARTICLES;
 
---
+++ www/js/data/HELPER_ARTICLES.js
@@ -0,0 +1,38 @@
+var HELPER_ARTICLES = {
+  hello: [
+    {
+      slug: 'how-to-learn',
+      title: '如何学好一门技术、语言'
+    },
+    {
+      slug: 'html',
+      title: '无处不在的HTML'
+    },
+    {
+      slug: 'css',
+      title: '无处不在的CSS'
+    },
+    {
+      slug: 'javascript',
+      title: '无处不在的JavaScript'
+    }
+  ],
+  env: [
+    {
+      slug: 'windows-unix',
+      title: 'Windows或者类Unix'
+    },
+    {
+      slug: 'pm',
+      title: '包管理'
+    },
+    {
+      slug: 'env-windows',
+      title: '搭建开发环境(Windows)'
+    },
+    {
+      slug: 'env-osx',
+      title: '搭建开发环境(OSX)'
+    }
+  ]
+};
