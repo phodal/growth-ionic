e29e7bb73cba99eb102898dc32bc3f1f5441dfba
Fengda HUANG <h@phodal.com>
2016-01-20 11:12:31 +0800

    add day1 artices

--- www/js/data/HELPER_ARTICLES.js
+++ www/js/data/HELPER_ARTICLES.js
@@ -34,5 +34,59 @@ var HELPER_ARTICLES = {
       slug: 'env-osx',
       title: '搭建开发环境(OSX)'
     }
+  ],
+  build: [
+    {
+      slug: 'git',
+      title: 'Git与版本管理'
+    },
+    {
+      slug: 'builder',
+      title: '一个构建系统的构建过程'
+    },
+    {
+      slug: 'browser-to-server',
+      title: '从浏览器到服务器发生了什么'
+    }
+  ],
+  front: [
+    {
+      slug: 'choice-frontend',
+      title: '如何选择前端框架'
+    },
+    {
+      slug: 'ajax',
+      title: 'Ajax与JSON'
+    },
+    {
+      slug: 'frontend-history',
+      title: '前端演进史'
+    },
+    {
+      slug: 'mvvm',
+      title: 'MVVM'
+    }
+  ],
+  mvc: [
+    {
+      slug: 'backend-language',
+      title: '如何选择后台语言?'
+    },
+    {
+      slug: 'mvc',
+      title: 'MVC'
+    },
+    {
+      slug: 'persistence',
+      title: '数据持久化'
+    },
+    {
+      slug: 'serviceful',
+      title: 'RESTful与服务化趋势'
+    },
+    {
+      slug: 'micro-services',
+      title: '微服务'
+    }
   ]
 };
