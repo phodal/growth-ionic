44782df727244042b22d480baf5c22cf550c2107
Fengda HUANG <h@phodal.com>
2016-01-24 18:24:02 +0800

    update make script

--- make.sh
+++ make.sh
@@ -3,3 +3,4 @@ cordova build --release android
 rm ~/repractise/growth/platforms/android/build/outputs/apk/growth.apk
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/phodal.keystore ~/repractise/growth/platforms/android/build/outputs/apk/android-release-unsigned.apk phodal
 ~/android-sdk/build-tools/22.0.0/zipalign -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk ~/repractise/growth/platforms/android/build/outputs/apk/growth.apk
+cp ~/repractise/growth/platforms/android/build/outputs/apk/growth.apk ~/Desktop/growth.apk
