3349f30a41cf79ee4e28bdab71831ffc06fef799
Fengda HUANG <h@phodal.com>
2016-01-24 16:55:05 +0800

    Get user Namees

--- www/js/controllers/CommunityCtrl.js
+++ www/js/controllers/CommunityCtrl.js
@@ -2,10 +2,16 @@ angular.module('starter.controllers')
   .controller('CommunityCtrl', function ($scope, Topics) {
     Topics.refresh().$promise.then(function (response) {
       $scope.topics = response.data;
-      });
+      $scope.included = response.included;
+    });
     $scope.doRefresh = function () {
       Topics.refresh().$promise.then(function (response) {
         $scope.topics = response.data;
+        $scope.included = response.included;
       });
+    };
+    $scope.getUserInfo = function (a) {
+      console.log(a);
+      return 'http://forum.growth.ren/assets/avatars/cnhnl3phpl8otwkh.jpg';
     }
   });
