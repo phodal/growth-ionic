cc8bd179af5e5e42ba07bd15b21228181bbbf60e
Fengda HUANG <h@phodal.com>
2016-01-25 22:51:09 +0800

    Add desktop version

--- desktop.js
+++ desktop.js
@@ -12,7 +12,7 @@ let mainWindow;
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow = new BrowserWindow({width: 1024, height: 768});
 
   // and load the index.html of the app.
   mainWindow.loadURL('file://' + __dirname + '/www/index.html');
