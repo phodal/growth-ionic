4036bc03e73547568d8a2760985bd2a55a4fadbd
Fengda HUANG <h@phodal.com>
2016-01-26 08:06:48 +0800

    add comment details

--- www/js/controllers/CommunityCtrl.js
+++ www/js/controllers/CommunityCtrl.js
@@ -62,5 +62,37 @@ angular.module('starter.controllers')
 
       $scope.post = $filter('filter')(response.included, {type: "posts", id: postId})[0];
       $scope.user = $filter('filter')(response.included, {type: "users"})[0];
+
+      $scope.discussion = response;
+      $scope.getUsername = function(user) {
+        var included = $scope.discussion.included;
+        for(var i=0; i < included.length; ++i) {
+          if(included[i].type === 'users' && included[i].id === user.data.id) {
+            return included[i].attributes.username;
+          }
+        }
+        return 'A user';
+      ;}
+
+      $scope.getAvatar = function(user) {
+        var included = $scope.discussion.included;
+        for(var i=0; i < included.length; ++i) {
+          if(included[i].type === 'users' && included[i].id === user.data.id && included[i].attributes.avatarUrl) {
+            return included[i].attributes.avatarUrl;
+          }
+        }
+      };
+
+      $scope.getUrl = function() {
+        var txtSrc = discussion.data.id + ' ' + discussion.data.attributes.title;
+        var output = txtSrc.replace(/[^a-zA-Z0-9]/g,' ').replace(/\s+/g,"-").toLowerCase();
+        /* remove first dash */
+        if(output.charAt(0) == '-') output = output.substring(1);
+        /* remove last dash */
+        var last = output.length-1;
+        if(output.charAt(last) == '-') output = output.substring(0, last);
+
+        return CONFIG.URL + 'd/' + output;
+      }
     });
   });
