33971ef04368f90385a5d3a99453e54c12247a07
Fengda HUANG <h@phodal.com>
2016-01-29 23:32:45 +0800

    add profile detail

--- www/js/controllers/community/ProfileCtrl.js
+++ www/js/controllers/community/ProfileCtrl.js
@@ -1,4 +1,8 @@
 angular.module('starter.controllers')
-  .controller('ProfileCtrl', function ($scope) {
-
+  .controller('ProfileCtrl', function ($scope, Users, $stateParams) {
+    Users.get({id: $stateParams.id}).$promise.then(function (response) {
+      $scope.userId = response.data.id;
+      $scope.userInfo = response.data.attributes;
+      $scope.addInfo = response.included;
+    });
   });
--- www/js/services/topics.js
+++ www/js/services/topics.js
@@ -7,13 +7,12 @@ angular.module('starter.services')
 
     return resource;
   })
-  .factory('Users', function ($resource, TokenHandler) {
+  .factory('Users', function ($resource) {
     var resource = $resource('http://forum.growth.ren/' + 'api/users/:id', {
       id: '@id'
     }, {
       update: {method: 'PUT'}
     });
 
-    resource = TokenHandler.wrapActions(resource, ['query', 'update', 'save', 'get', 'remove', 'delete']);
     return resource;
   });
