ccdbab38d29611dded18d03027454d0c439af0f5
Fengda HUANG <h@phodal.com>
2016-02-09 15:18:46 +0800

    format code

--- www/js/controllers/MoreCtrl.js
+++ www/js/controllers/MoreCtrl.js
@@ -26,10 +26,10 @@ angular.module('starter.controllers')
     $scope.share = function () {
       var growthHomepage = 'http://growth.ren/';
       $cordovaSocialSharing
-        .share('我现在使用Growth，这是一款专注于Web开发者成长的应用，涵盖Web开发的流程及技术栈，Web开发的学习路线、成长衡量等各方面。快来下载吧!', '', '', growthHomepage) // Share via native share sheet
-        .then(function(result) {
+        .share('我现在使用Growth，这是一款专注于Web开发者成长的应用，涵盖Web开发的流程及技术栈，Web开发的学习路线、成长衡量等各方面。快来下载吧!', '', '', growthHomepage)
+        .then(function (result) {
           // Success!
-        }, function(err) {
+        }, function (err) {
           // An error occured. Show a message to the user
         });
     }
