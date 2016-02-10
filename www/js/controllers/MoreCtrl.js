208165d86743a0f55f4cc6daf9d59f89beb8b7d5
Fengda HUANG <h@phodal.com>
2016-02-10 09:14:01 +0800

    add track event

--- www/js/controllers/MoreCtrl.js
+++ www/js/controllers/MoreCtrl.js
@@ -1,5 +1,7 @@
 angular.module('starter.controllers')
-  .controller('MoreCtrl', function ($scope, $ionicTabsDelegate, $cordovaAppRate, $cordovaSocialSharing) {
+  .controller('MoreCtrl', function ($scope, $ionicTabsDelegate, $cordovaAppRate, $cordovaSocialSharing, $analytics) {
+    $analytics.trackView('More Ctrl');
+
     $scope.goForward = function () {
       var selected = $ionicTabsDelegate.selectedIndex();
       if (selected != -1) {
@@ -16,11 +18,8 @@ angular.module('starter.controllers')
 
     $scope.rating = function () {
       $cordovaAppRate.navigateToAppStore().then(function (result) {
-        // success
+        $analytics.trackView('More', 'Rate', result);
       });
-      //$cordovaAppRate.promptForRating(true).then(function (result) {
-      //  // success
-      //});
     };
 
     $scope.share = function () {
@@ -28,9 +27,9 @@ angular.module('starter.controllers')
       $cordovaSocialSharing
         .share('我现在使用Growth，这是一款专注于Web开发者成长的应用，涵盖Web开发的流程及技术栈，Web开发的学习路线、成长衡量等各方面。快来下载吧!', '', '', growthHomepage)
         .then(function (result) {
-          // Success!
+          $analytics.trackView('More', 'Share', result);
         }, function (err) {
-          // An error occured. Show a message to the user
+          $analytics.trackView('More', 'Share Error', error);
         });
     }
   });
