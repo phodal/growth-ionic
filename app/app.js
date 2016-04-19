e4412cee42d3fd66a78036d8a30edde02d626914
Fengda HUANG <h@phodal.com>
2016-04-19 21:59:53 +0800

    create main page

--- app/app.js
+++ app/app.js
@@ -20,6 +20,7 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
+      StatusBar.backgroundColorByHexString("#5e6772");
     });
   }
 }
