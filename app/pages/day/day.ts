f005383a05f1d63d828683bbc6482dcbcbbe3015
Fengda HUANG <h@phodal.com>
2016-07-11 21:27:31 +0800

    create day pages

---
+++ app/pages/day/day.ts
@@ -0,0 +1,50 @@
+import {NavController, ModalController} from "ionic-angular";
+import {Component} from "@angular/core";
+import {HELPER_ARTICLES} from "../../data/HELPER_ARTICLES";
+import {TODO_LISTS} from "../../data/TODO_LISTS";
+import {Todo} from "../../components/todo/todo";
+import {ArticleModal} from "../../components/article/article";
+
+@Component({
+  templateUrl: 'build/pages/day/day1.html'
+})
+export class Day1 {
+  basicView:string = "articleView";
+  articles = HELPER_ARTICLES['zh-cn'];
+
+  constructor(public nav:NavController, private modalCtrl:ModalController) {
+    this.nav = nav;
+    this.modalCtrl = modalCtrl;
+  }
+
+  ionViewLoaded() {
+    console.log("I'm alive!");
+  }
+
+  ionViewWillLeave() {
+    console.log("Looks like I'm about to leave :(");
+  }
+
+  presentTodoModal(params) {
+    var todoLists = TODO_LISTS['zh-cn'][params.domain];
+    var todoModal = this.modalCtrl.create(Todo, {todoLists: todoLists});
+    todoModal.present();
+  }
+
+  presentArticleModal(params) {
+    var articleModal;
+
+    if (params.type === 'desc') {
+      var slug = 'assets/desc/html/' + params.slug + '.html';
+      articleModal = this.modalCtrl.create(ArticleModal, {slug: slug});
+    } else if (params.domain) {
+      var slug = 'assets/growth/' + params.domain + '/' + params.slug + '.html';
+      articleModal = this.modalCtrl.create(ArticleModal, {slug: slug});
+    } else {
+      var slug = 'assets/articles/' + params.slug + '.html';
+      articleModal = this.modalCtrl.create(ArticleModal, {slug: slug});
+    }
+
+    articleModal.present();
+  }
+}
