6d10dde8f5348c3538d84529caef387bad4a1509
Fengda HUANG <h@phodal.com>
2016-07-12 20:40:07 +0800

    [T] add cordova for typescripts

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,4 @@
+/// <reference path="globals/cordova/index.d.ts" />
 /// <reference path="globals/es6-shim/index.d.ts" />
 /// <reference path="globals/hammerjs/index.d.ts" />
 /// <reference path="globals/jasmine/index.d.ts" />
