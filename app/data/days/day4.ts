98c3dbc2664c5f5ec013678cf8b2665c56328e7f
Fengda HUANG <h@phodal.com>
2016-07-13 07:51:50 +0800

    add day 4

--- app/data/days/day4.ts
+++ app/data/days/day4.ts
@@ -3,30 +3,54 @@ export const DAYS4 = {
   title: '数据分析',
   intros: [
     {
-      info: {slug: 'front', type: 'desc'}
+      info: {slug: 'analytics', type: 'desc'}
+    },
+    {
+      info: {slug: 'ux', type: 'desc'}
     }
   ],
   articlesView: [
     {
-      title: '构建系统',
-      articles: HELPER_ARTICLES['zh-cn'].build
+      title: '数据分析',
+      articles: HELPER_ARTICLES['zh-cn'].analytics
+    },
+    {
+      title: '用户体验',
+      articles: HELPER_ARTICLES['zh-cn'].ux
     }
   ],
   growthView: [
     {
-      title: '前端',
+      title: '数据分析',
       sections: [
         {
-          title: 'Debug',
-          info: {slug: 'debug', type: 'general', domain: 'frontend'}
+          title: '书单',
+          info: {type: 'book', domain: 'analytics'}
+        },
+        {
+          title: '工具',
+          info: {type: 'book', domain: 'analytics'}
+        }
+      ]
+    },
+    {
+      title: '用户体验',
+      sections: [
+        {
+          title: '书单',
+          info: {type: 'book', domain: 'ux'}
+        },
+        {
+          title: 'Growth',
+          info: {type: 'book', domain: 'ux'}
         }
       ]
     }
   ],
   todoView: [
     {
-      title: '前端',
-      info: {domain: 'frontend'}
+      title: '数据分析',
+      info: {domain: 'analytics'}
     }
   ]
 };
