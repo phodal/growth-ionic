19dee9d0fe29028f10212ff26d56bdc8733ca307
Fengda HUANG <h@phodal.com>
2016-07-14 21:45:22 +0800

    add more e2e test for hoempage

--- app/app.e2e.ts
+++ app/app.e2e.ts
@@ -1,18 +1,31 @@
-describe('MyAPP', () => {
+describe("MyAPP", () => {
+  browser.get("");
+  //
+  // beforeEach(() => {
+  //
+  // });
 
-  beforeEach(() => {
-    browser.get('');
+  it("should have a title", () => {
+    expect(browser.getTitle()).toEqual("Growth");
   });
 
-  it('should have a title', () => {
-    expect(browser.getTitle()).toEqual('Growth');
+  it("should have <nav>", () => {
+    expect(element(by.css("ion-navbar")).isPresent()).toEqual(true);
   });
 
-  it('should have <nav>', () => {
-    expect(element(by.css('ion-navbar')).isPresent()).toEqual(true);
+  it("should have correct nav text for Home", () => {
+    expect(element(by.css("ion-navbar:first-child")).getText()).toContain("Growth");
   });
 
-  it('should have correct nav text for Home', () => {
-    expect(element(by.css('ion-navbar:first-child')).getText()).toContain('Growth');
+  xit("should have correct slide bar for Home", () => {
+    expect(element(by.css("ion-slide h2")).getText()).toContain("《Growth：全栈增长工程师指南》");
+  });
+
+  it("should have correct day for Home", () => {
+    expect(element(by.css("ion-card.days ion-label h2")).getText()).toContain("从零开始");
+  });
+
+  it("should have correct footer Home", () => {
+    expect(element(by.css("ion-tabbar a span")).getText()).toContain("GROWTH");
   });
 });
