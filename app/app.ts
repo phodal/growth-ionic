f638e60461c64a48dd6f6f79fb5927ff8fb77272
Fengda HUANG <h@phodal.com>
2016-07-17 23:47:58 +0800

    fix tslint

--- app/app.ts
+++ app/app.ts
@@ -2,7 +2,7 @@ import {Platform, ionicBootstrap} from "ionic-angular";
 import {StatusBar} from "ionic-native";
 import {TabsPage} from "./pages/tabs/tabs";
 import {Component} from "@angular/core";
-import '../node_modules/chart.js/dist/Chart.bundle.min.js';
+import "../node_modules/chart.js/dist/Chart.bundle.min.js";
 
 @Component({
   template: "<ion-nav [root]='rootPage'></ion-nav>"
--- app/pages/analytics/index.ts
+++ app/pages/analytics/index.ts
@@ -10,6 +10,17 @@ import {CHART_DIRECTIVES} from "ng2-charts/ng2-charts";
   directives: [CHART_DIRECTIVES, NgClass, CORE_DIRECTIVES, FORM_DIRECTIVES]
 })
 export class Analytics {
+  public radarChartOptions:any = {
+    scaleShowVerticalLines: false,
+    responsive: false
+  };
+  public radarChartType:string = "radar";
+  public radarChartLabels:string[] = ["Coding", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"];
+  public radarChartData:any = [
+    {data: [65, 59, 90, 81, 56, 55, 40], label: "Series A"},
+    {data: [28, 48, 40, 19, 96, 27, 100], label: "Series B"}
+  ];
+
   private skillPointAmount;
 
   constructor(public skillMapService:SkillMapService) {
@@ -23,25 +34,12 @@ export class Analytics {
     });
   }
 
-  public radarChartOptions:any = {
-    scaleShowVerticalLines: false,
-    responsive: false
-  };
-
-  public radarChartLabels:string[] = ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'];
-
-  public radarChartData:any = [
-    {data: [65, 59, 90, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 96, 27, 100], label: 'Series B'}
-  ];
-  public radarChartType:string = 'radar';
-
-  // events
-  public chartClicked(e:any):void {
-    console.log(e);
-  }
-
-  public chartHovered(e:any):void {
-    console.log(e);
-  }
+  // // events
+  // public chartClicked(e:any):void {
+  //   console.log(e);
+  // }
+  //
+  // public chartHovered(e:any):void {
+  //   console.log(e);
+  // }
 }
