b0da86cbc7494460c55dec04397f345253d06b1a
Fengda HUANG <h@phodal.com>
2016-07-18 07:54:39 +0800

    [skill-map] add basic template for skill map

--- app/pages/analytics/skill-list/index.ts
+++ app/pages/analytics/skill-list/index.ts
@@ -1,10 +1,17 @@
 import {Component} from "@angular/core";
+import {ALL_SKILLS} from "../../../data/ALL_SKILLS";
 
 @Component({
   templateUrl: "build/pages/analytics/skill-list/index.html"
 })
 export class SkillListPage {
+  private allSkills;
+
   constructor() {
+    this.allSkills = ALL_SKILLS;
+  }
+
+  setStar(skill, $event) {
 
   }
 }
