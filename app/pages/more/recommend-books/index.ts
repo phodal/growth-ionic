f4881a3d85fba63ba24e795eecf45a69026ea226
Fengda HUANG <h@phodal.com>
2016-07-18 23:01:11 +0800

    [more] add recommend books

--- app/pages/more/recommend-books/index.ts
+++ app/pages/more/recommend-books/index.ts
@@ -1,10 +1,12 @@
 import {Component} from "@angular/core";
+import {BOOKS} from "../../../data/BOOKS";
 
 @Component({
   templateUrl: "build/pages/more/recommend-books/index.html"
 })
 export class RecommendBook {
+  private books;
   constructor() {
-
+    this.books = BOOKS;
   }
 }
