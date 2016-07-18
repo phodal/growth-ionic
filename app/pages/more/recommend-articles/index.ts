6cae1b5acbffbf84c56f18b5a0aa3e5331e1defe
Fengda HUANG <h@phodal.com>
2016-07-19 07:47:14 +0800

    [recommend-books] make user can go to article detail page

--- app/pages/more/recommend-articles/index.ts
+++ app/pages/more/recommend-articles/index.ts
@@ -2,20 +2,37 @@ import {Component} from "@angular/core";
 import * as _ from "lodash";
 import {HELPER_ARTICLES} from "../../../data/HELPER_ARTICLES";
 import {DOMAIN} from "../../../data/DOMAIN_NAME";
+import {HtmlModal} from "../../../modals/HtmlModal/index";
+import {ModalController} from "ionic-angular/index";
 
 @Component({
   templateUrl: "build/pages/more/recommend-articles/index.html"
 })
 export class RecommendArticles {
   private helpArticles;
-  constructor() {
-    this.helpArticles = _.transform(HELPER_ARTICLES["zh-cn"], function(result, value, key) {
+
+  constructor(private modalCtrl:ModalController) {
+    this.modalCtrl = modalCtrl;
+    this.init();
+  }
+
+  private init() {
+    this.helpArticles = _.transform(HELPER_ARTICLES["zh-cn"], function (result, value, key) {
       result.push({domain: key, value: value});
       return true;
     }, []);
   }
 
-  getDomainName (domain) {
+  getDomainName(domain) {
     return DOMAIN[domain];
   }
+
+  presentHtmlModal(params) {
+    let htmlModal, modalParams;
+    let slug = "assets/article/" + params.slug + ".html";
+    modalParams = {slug: slug, pageTitle: "文章"};
+    htmlModal = this.modalCtrl.create(HtmlModal, modalParams);
+    htmlModal.present();
+  }
+
 }
