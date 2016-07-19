442b170ed3a69606ac6ac1bb8f74255bff407eb0
Fengda HUANG <h@phodal.com>
2016-07-19 23:19:49 +0800

    [book] add detail modal for recommend book

--- app/pages/more/recommend-books/index.ts
+++ app/pages/more/recommend-books/index.ts
@@ -1,13 +1,23 @@
 import {Component} from "@angular/core";
 import {BOOKS} from "../../../data/BOOKS";
 import * as _ from "lodash";
+import {ModalController} from "ionic-angular/index";
+import {HtmlModal} from "../../../modals/HtmlModal/index";
 
 @Component({
   templateUrl: "build/pages/more/recommend-books/index.html"
 })
 export class RecommendBook {
   private books;
-  constructor() {
+  constructor(private modalCtrl:ModalController) {
+    this.modalCtrl = modalCtrl;
     this.books = _.orderBy(BOOKS, ["category"], ["asc"]);
+  }
+
+  presentBookDetail(slug) {
+    let modalParams, htmlModal;
+    modalParams = {slug: "assets/review/" + slug + ".html", pageTitle: "书评"};
+    htmlModal = this.modalCtrl.create(HtmlModal, modalParams);
+    htmlModal.present();
   }
 }
