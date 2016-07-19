566fa4aca2d223dd3e0704c074c1bbf70a0aa3cc
Fengda HUANG <h@phodal.com>
2016-07-19 23:08:47 +0800

    [skill-map] try to add test

--- app/services/skill.map.services.spec.ts
+++ app/services/skill.map.services.spec.ts
@@ -1,30 +1,25 @@
-import {Storage, LocalStorage} from "ionic-angular/index";
 import {SkillMapService} from "./skill.map.services";
+import {inject} from "@angular/core/testing";
 
 describe("Skill Map Services", () => {
   let skillMapService;
 
   beforeEach(() => {
-    let localStorage = new Storage(LocalStorage);
     skillMapService = new SkillMapService();
-
-    localStorage.set("skills", {
-      "Npm": 5,
-      "Grunt": 4,
-      "Make": 5,
-      "Gradle": 5,
-      "JavaScript": 5,
-      "HTML5": 5,
-      "Java": 5,
-      "Python": 5
-    });
   });
 
-  xit("should able add skill to localstorage", function (done) {
+  // TODO fix tests
+  it("should able add skill to get skills by domain", inject([], () => {
+    spyOn(localStorage, "setItem");
+    skillMapService.addSkill({skill: "photoshop", rate: "5"});
+    // expect(localStorage.setItem).toHaveBeenCalledWith("skills", "newToken");
+  }));
+
+  // TODO fix tests
+  it("should able add skill to localstorage", () => {
     skillMapService.addSkill({skill: "photoshop", rate: "5"});
     skillMapService.getSkills().then(function (data) {
-      expect(JSON.stringify(JSON.parse(data))).toBe("");
-      done();
+      expect(JSON.parse(data)).toBe(null);
     });
   });
 });
