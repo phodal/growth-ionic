5b628530528b05961f015687e04874c5fb764065
Fengda HUANG <h@phodal.com>
2016-07-19 23:17:05 +0800

    [books] add present book detal modal

--- app/modals/BookListModal/index.ts
+++ app/modals/BookListModal/index.ts
@@ -1,6 +1,7 @@
-import {NavParams, ViewController, Platform} from "ionic-angular/index";
+import {NavParams, ViewController, Platform, ModalController} from "ionic-angular/index";
 import {Component} from "@angular/core";
 import {BOOKS} from "../../data/BOOKS";
+import {HtmlModal} from "../HtmlModal/index";
 
 @Component({
   templateUrl: "build/modals/BookListModal/index.html"
@@ -10,13 +11,23 @@ export class BookListModal {
 
   constructor(public platform:Platform,
               public params:NavParams,
+              private modalCtrl:ModalController,
               public viewCtrl:ViewController) {
     let domain = params.get("domain");
+
+    this.modalCtrl = modalCtrl;
     this.books = BOOKS.filter(function(el){
       return el.category === domain;
     });
   }
   dismiss() {
     this.viewCtrl.dismiss();
+  }
+
+  presentBookDetail(slug) {
+    let modalParams, htmlModal;
+    modalParams = {slug: "assets/review/" + slug + ".html", pageTitle: "书评"};
+    htmlModal = this.modalCtrl.create(HtmlModal, modalParams);
+    htmlModal.present();
   }
 }
