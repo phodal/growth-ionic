b961f21de79b7bfb575371aa0b4a3b0790f778a2
Fengda HUANG <h@phodal.com>
2016-07-21 07:23:35 +0800

    [solution] add detail page

--- app/pages/solution/index.ts
+++ app/pages/solution/index.ts
@@ -1,6 +1,8 @@
 import {Component} from "@angular/core";
 import {SOLUTIONS} from "../../data/SOLUTIONS";
 import * as _ from "lodash";
+import {HtmlModal} from "../../modals/HtmlModal/index";
+import {ModalController} from "ionic-angular/index";
 
 @Component({
   templateUrl: "build/pages/solution/index.html",
@@ -8,7 +10,15 @@ import * as _ from "lodash";
 export class SolutionPage {
   private solutions;
 
-  constructor() {
+  constructor(private modalCtrl:ModalController) {
     this.solutions = _.orderBy(SOLUTIONS, ["type"], ["asc"]);
+    this.modalCtrl = modalCtrl;
+  }
+
+  presentSolutionDetail(slug) {
+    let modalParams, htmlModal;
+    modalParams = {slug: "assets/solution/" + slug + ".html", pageTitle: "解决方案"};
+    htmlModal = this.modalCtrl.create(HtmlModal, modalParams);
+    htmlModal.present();
   }
 }
