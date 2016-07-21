1f1397b309e27d65f10d8cfcbff1493374b7f832
Fengda HUANG <h@phodal.com>
2016-07-22 07:46:01 +0800

    create my bookmark page

--- app/pages/user-center/index.ts
+++ app/pages/user-center/index.ts
@@ -2,7 +2,7 @@ import {Component} from "@angular/core";
 import {NavController} from "ionic-angular/index";
 
 @Component({
-  templateUrl: "build/pages/more/index.html"
+  templateUrl: "build/pages/user-center/index.html"
 })
 export class UserCenterPage {
   constructor(public nav:NavController) {
