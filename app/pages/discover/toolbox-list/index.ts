ebc8995b95ecd4e66fca1ed3d27abce3d74f9cce
Fengda HUANG <h@phodal.com>
2016-07-22 16:41:44 +0800

    [toolbox] add loading effect

--- app/pages/discover/toolbox-list/index.ts
+++ app/pages/discover/toolbox-list/index.ts
@@ -1,6 +1,7 @@
 import {Component} from "@angular/core";
-import {NavController} from "ionic-angular/index";
+import {NavController, LoadingController} from "ionic-angular/index";
 import {Http, HTTP_PROVIDERS} from "@angular/http";
+import 'rxjs/add/operator/map';
 
 @Component({
   templateUrl: "build/pages/discover/toolbox-list/index.html",
@@ -10,12 +11,19 @@ import {Http, HTTP_PROVIDERS} from "@angular/http";
 export class ToolboxListPage {
   private toolboxs;
 
-  constructor(public nav:NavController, public http:Http) {
+  constructor(private loadingCtrl:LoadingController, public nav:NavController, public http:Http) {
     this.http = http;
     this.init();
   }
 
   private init() {
+    let loading = this.loadingCtrl.create({
+      spinner: 'circles',
+      content: `<ion-spinner [name]="d.spinner"></ion-spinner>`,
+      duration: 3000
+    });
+    loading.present();
+
     let url = 'http://toolbox.phodal.com/api/all.json';
     let self = this;
     this.http.get(url)
@@ -23,6 +31,7 @@ export class ToolboxListPage {
       .subscribe(
         data => {
           self.toolboxs = data["content"];
+          loading.dismiss();
         },
         err => {
           console.log(err);
