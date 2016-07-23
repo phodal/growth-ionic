f071f471144938b6a7618fab00d9dc19402f5bc1
Fengda HUANG <h@phodal.com>
2016-07-23 14:35:34 +0800

    update loading timeout times

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -2,7 +2,7 @@ export const ANIMATION_DURATION = 500;
 export const SPINNER_CONFIG = {
   spinner: "circles",
   content: `<ion-spinner [name]="d.spinner"></ion-spinner>`,
-  duration: 10000
+  duration: 20000
 };
 export const SERVER_BASE_URL = {
   toolbox: "http://toolbox.phodal.com/"
