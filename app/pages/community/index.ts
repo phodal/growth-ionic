b72a5b6d729db5b29cbf9ed6f2eea4eed48b7767
Fengda HUANG <h@phodal.com>
2016-07-25 22:51:37 +0800

    add community api for list page

--- app/pages/community/index.ts
+++ app/pages/community/index.ts
@@ -1,10 +1,35 @@
 import {Component} from "@angular/core";
+import {LoadingController, NavController} from "ionic-angular/index";
+import {Http} from "@angular/http";
+import {getSpinnerConfig} from "../../utils/helper";
+import {SERVER_BASE_URL} from "../../utils/constants";
 
 @Component({
   templateUrl: "build/pages/community/index.html"
 })
 export class CommunityPage {
-  constructor() {
+  private topics;
+  private included;
 
+  constructor(private loadingCtrl:LoadingController, public nav:NavController, public http:Http) {
+    this.http = http;
+    this.init();
+  }
+
+  init() {
+    let loading = this.loadingCtrl.create(getSpinnerConfig());
+    loading.present();
+
+    let url = SERVER_BASE_URL.forum;
+    let self = this;
+    this.http.get(url)
+      .map(res => res.json())
+      .subscribe(
+        data => {
+          self.topics = data.data;
+          self.included = data.included;
+          loading.dismiss();
+        }
+      );
   }
 }
