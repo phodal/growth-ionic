a4bbeaf2eed5aaa5b7079b80c6c9307664017d44
Fengda HUANG <h@phodal.com>
2016-07-26 07:31:03 +0800

    [community] add load more

--- app/pages/community/index.ts
+++ app/pages/community/index.ts
@@ -5,6 +5,7 @@ import {getSpinnerConfig} from "../../utils/helper";
 import {SERVER_BASE_URL} from "../../utils/constants";
 import {TimeAgoPipe} from "angular2-moment";
 import "moment/locale/zh-cn";
+import {mergeWith} from "lodash";
 
 @Component({
   templateUrl: "build/pages/community/index.html",
@@ -14,6 +15,7 @@ import "moment/locale/zh-cn";
 export class CommunityPage {
   private topics;
   private included;
+  private nextPageUrl;
 
   constructor(private loadingCtrl:LoadingController, public nav:NavController, public http:Http) {
     this.http = http;
@@ -21,18 +23,44 @@ export class CommunityPage {
   }
 
   init() {
+    let url = SERVER_BASE_URL.forum;
+    let self = this;
     let loading = this.loadingCtrl.create(getSpinnerConfig());
     loading.present();
 
-    let url = SERVER_BASE_URL.forum;
-    let self = this;
     this.http.get(url)
       .map(res => res.json())
       .subscribe(
         data => {
           self.topics = data.data;
           self.included = data.included;
+          if (data.links && data.links.next) {
+            self.nextPageUrl = data.links.next;
+          } else {
+            self.nextPageUrl = null;
+          }
+
           loading.dismiss();
+        }
+      );
+  }
+
+  doInfinite(infiniteScroll, url) {
+    console.log('Begin async operation');
+    let self = this;
+    this.http.get(url)
+      .map(res => res.json())
+      .subscribe(
+        data => {
+          console.log(data.data);
+          self.topics = mergeWith(self.topics, data.data);
+          self.included = mergeWith(self.topics, data.included);
+          if (data.links && data.links.next) {
+            self.nextPageUrl = data.links.next;
+          } else {
+            self.nextPageUrl = null;
+          }
+          infiniteScroll.complete();
         }
       );
   }
