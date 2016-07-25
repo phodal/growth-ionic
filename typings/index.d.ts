43fc39ab6c16e52f9085d9fce1aa29a57cbbc79e
Fengda HUANG <h@phodal.com>
2016-07-25 22:51:56 +0800

    remove showdown from typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -8,4 +8,5 @@
 /// <reference path="globals/marked/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
 /// <reference path="globals/selenium-webdriver/index.d.ts" />
+/// <reference path="globals/showdown/index.d.ts" />
 /// <reference path="modules/lodash/index.d.ts" />
