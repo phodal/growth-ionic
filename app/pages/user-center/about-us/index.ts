20bcab0f9e903e0940644bfbeae1dc963d24a8ac
Fengda HUANG <h@phodal.com>
2016-07-27 14:22:42 +0800

    [more] add app version script

--- app/pages/user-center/about-us/index.ts
+++ app/pages/user-center/about-us/index.ts
@@ -2,6 +2,7 @@ import {Component} from "@angular/core";
 import {CONTRIBUTORS} from "../../../data/CONTRIBUTORS";
 import {NavController} from "ionic-angular/index";
 import {Profile} from "./profile/index";
+import {AppVersion} from "ionic-native/dist/index";
 
 @Component({
   templateUrl: "build/pages/user-center/about-us/index.html"
@@ -9,12 +10,24 @@ import {Profile} from "./profile/index";
 
 export class AboutUsPage {
   private contributors;
+  private version;
+
   constructor(public nav:NavController) {
     this.nav = nav;
     this.contributors = CONTRIBUTORS;
+    this.init();
   }
 
   openProfilePage(num) {
     this.nav.push(Profile, {num: num});
+  }
+
+  private init() {
+    if (window.cordova) {
+      let self = this;
+      AppVersion.getVersionNumber().then(
+        version => self.version = version
+      );
+    }
   }
 }
