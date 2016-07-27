8ff82db41b2cd72adbe4876c0f2a9de718a53caa
Fengda HUANG <h@phodal.com>
2016-07-27 15:21:21 +0800

    [ui] remove statusbar color

--- app/app.ts
+++ app/app.ts
@@ -23,8 +23,6 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       this.hideSplashScreen();
-      StatusBar.styleDefault();
-      StatusBar.backgroundColorByHexString("#5e6772");
     });
   }
   private hideSplashScreen() {
