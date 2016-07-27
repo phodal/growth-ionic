25e0c876a3fac02ab25a40467c6296d5aa9a65a9
Fengda HUANG <h@phodal.com>
2016-07-27 11:40:55 +0800

    fix test

--- app/app.spec.ts
+++ app/app.spec.ts
@@ -1,8 +1,9 @@
 import {
-  TEST_BROWSER_DYNAMIC_APPLICATION_PROVIDERS, TEST_BROWSER_DYNAMIC_PLATFORM_PROVIDERS,
-}                               from "@angular/platform-browser-dynamic/testing";
+  TEST_BROWSER_DYNAMIC_APPLICATION_PROVIDERS,
+  TEST_BROWSER_DYNAMIC_PLATFORM_PROVIDERS
+} from "@angular/platform-browser-dynamic/testing";
 import {setBaseTestProviders} from "@angular/core/testing";
-import {MyApp}           from "./app";
+import {MyApp} from "./app";
 
 setBaseTestProviders(TEST_BROWSER_DYNAMIC_PLATFORM_PROVIDERS, TEST_BROWSER_DYNAMIC_APPLICATION_PROVIDERS);
 
@@ -22,13 +23,17 @@ class MockClass {
   public setRoot():any {
     return true;
   }
+
+  public logout():any {
+    return true;
+  }
 }
 
 describe("MyAPP", () => {
 
   beforeEach(() => {
     let mockClass:any = (<any>new MockClass());
-    myApp = new MyApp(mockClass);
+    myApp = new MyApp(mockClass, mockClass);
   });
 
   it("initialises with a root page", () => {
