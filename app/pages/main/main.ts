05707b35511636f3d92d8067190e27eff7f33dd8
Fengda HUANG <h@phodal.com>
2016-07-28 21:53:06 +0800

    [ui] remove slide option

--- app/pages/main/main.ts
+++ app/pages/main/main.ts
@@ -7,23 +7,6 @@ import {Day} from "./day/day";
   templateUrl: "build/pages/main/main.html"
 })
 export class MainView {
-  private slides = [
-    {
-      title: "《Growth：全栈增长工程师实战》",
-      intro: "在Growth中我们介绍的只是一系列的实践，而Growth实战则会带领读者去履行这些实践。",
-      action: "growthAction"
-    },
-    {
-      title: "《Ideabook：练手项目集》",
-      intro: "这个电子书的目标就是为这些Idea提供实战指南，一步步搭建你的未来。",
-      action: "ideabook"
-    }
-  ];
-  private slideOptions = {
-    initialSlide: 0,
-    autoplay: 5000,
-    loop: true
-  };
   private shownGroup = false;
 
   constructor(public nav:NavController, private platform:Platform) {
