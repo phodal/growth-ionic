6cd5de2388ac784402c3d3553f86685cedba7b8c
Fengda HUANG <h@phodal.com>
2016-07-29 14:17:59 +0800

    [analytics] add back for analytics

--- app/services/analytics.services.ts
+++ app/services/analytics.services.ts
@@ -3,31 +3,29 @@ import {GoogleAnalytics} from "ionic-native/dist/index";
 
 @Injectable()
 export class AnalyticsServices {
-  private analytics;
 
   constructor() {
-    this.analytics = new GoogleAnalytics();
   };
 
   initID() {
-    this.analytics.startTrackerWithId("UA-71907748-1");
+    GoogleAnalytics.startTrackerWithId("UA-71907748-1");
   }
 
   trackView(view) {
     if (window.cordova) {
-      // this.initID();
-      // this.analytics.trackView(view);
+      this.initID();
+      GoogleAnalytics.trackView(view);
     } else {
-      // console.log("Analytics Track: " + view);
+      console.log("Analytics Track: " + view);
     }
   }
 
   trackEvent(category, action) {
     if (window.cordova) {
-      // this.initID();
-      // this.analytics.trackEvent(category, action);
+      this.initID();
+      GoogleAnalytics.trackEvent(category, action);
     } else {
-      // console.log("Analytics Event: " + category + " Action: " + action);
+      console.log("Analytics Event: " + category + " Action: " + action);
     }
   }
 
