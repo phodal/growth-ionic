1e361f3bfc6d77a147414025db13fde23034b102
Fengda HUANG <h@phodal.com>
2016-08-07 09:35:53 +0800

    update build config

--- make.sh
+++ make.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cordova build --release android
-rm ~/learing/growth2/platforms/android/build/outputs/apk/growth.apk
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/phodal.keystore ~/learing/growth2/platforms/android/build/outputs/apk/android-release-unsigned.apk phodal
-~/android-sdk/build-tools/22.0.0/zipalign -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk ~/learing/growth2/platforms/android/build/outputs/apk/growth.apk
-cp ~/learing/growth2/platforms/android/build/outputs/apk/growth.apk ~/Desktop/growth.apk
+rm ~/repractise/growth/platforms/android/build/outputs/apk/growth.apk
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/phodal.keystore ~/repractise/growth/platforms/android/build/outputs/apk/android-release-unsigned.apk phodal
+~/android-sdk/build-tools/23.0.1/zipalign -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk ~/repractise/growth/platforms/android/build/outputs/apk/growth.apk
+cp ~/repractise/growth/platforms/android/build/outputs/apk/growth.apk ~/Desktop/growth.apk
