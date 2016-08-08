3883861a7c82121d2ac0b0163d385f35440edb83
Fengda HUANG <h@phodal.com>
2016-08-09 07:41:04 +0800

    [tutorial use icon as title ui

--- app/pages/tutorial/tutorial.ts
+++ app/pages/tutorial/tutorial.ts
@@ -6,6 +6,7 @@ interface Slide {
   title: string;
   description: string;
   image: string;
+  icon: string;
 }
 
 @Component({
@@ -21,21 +22,25 @@ export class TutorialPage {
         title: "欢迎使用 <b>Growth 2</b>",
         description: "带你变成优秀开发者，陪你成为顶尖开发者",
         image: "images/slide.png",
+        icon: ""
       },
       {
         title: "Growth",
         description: "<b>Growth</b>视图可以帮助你构建你的知识体系——建议你学习什么，阅读什么相关书籍",
-        image: "images/slide.png",
+        image: "",
+        icon: "ios-home-outline"
       },
       {
         title: "探索",
         description: "Discover视图下包含学习路线、练手项目、工具箱、在线文章，结合Growth可以帮你巩固你的技能。",
-        image: "images/slide.png",
+        image: "",
+        icon: "ios-compass-outline"
       },
       {
         title: "社区",
         description: "除了使用APP来访问社区的功能，你还可以在线使用：http://forum.growth.ren/",
-        image: "images/slide.png",
+        image: "",
+        icon: "ios-people-outline"
       }
     ];
   }
