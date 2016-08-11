0b0d59f199e577155f12482911371d273cac613d
Fengda HUANG <h@phodal.com>
2016-08-12 07:28:11 +0800

    update CONTRIBUTORS.ts

--- app/data/CONTRIBUTORS.ts
+++ app/data/CONTRIBUTORS.ts
@@ -9,17 +9,52 @@ export const CONTRIBUTORS = [
     "blog": "https://www.phodal.com/"
   },
   {
+    "nickname": "Song",
+    "duty": "UX",
+    "bio": "重新设计Growth界面",
+    "github": "song-what"
+  },
+  {
     "nickname": "haoflynet",
     "duty": "dev",
     "github": "haoflynet",
-    "bio": "Change My World by Program",
+    "bio": "添加关于我们页面",
     "weibo": "豪翔天下",
     "blog": "https://haofly.net"
   },
   {
-    "nickname": "Song",
-    "duty": "UX",
-    "bio": "re-design Growth",
-    "github": "song-what"
-  }
+    "nickname": "raydeng83",
+    "duty": "dev",
+    "github": "raydeng83",
+    "bio": "添加1.0版翻译",
+    "weibo": ""
+  },
+  {
+    "nickname": "LightQueue",
+    "duty": "dev",
+    "github": "LightQueue",
+    "bio": "",
+    "weibo": ""
+  },
+  {
+    "nickname": "JasJia",
+    "duty": "dev",
+    "github": "JasJia",
+    "bio": "",
+    "weibo": ""
+  },
+  {
+    "nickname": "zhangmx",
+    "duty": "dev",
+    "github": "zhangmx",
+    "bio": "",
+    "weibo": ""
+  },
+  {
+    "nickname": "JimmyLv",
+    "duty": "dev",
+    "github": "JimmyLv",
+    "bio": "",
+    "weibo": ""
+  },
 ];
