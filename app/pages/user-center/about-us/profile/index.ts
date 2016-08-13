fc3a90907c13e20047d4d93515599d45076f712d
Fengda HUANG <h@phodal.com>
2016-08-13 20:57:56 +0800

    move profile link to controller

--- app/pages/user-center/about-us/profile/index.ts
+++ app/pages/user-center/about-us/profile/index.ts
@@ -1,6 +1,7 @@
 import {NavParams} from "ionic-angular";
 import {Component} from "@angular/core";
 import {CONTRIBUTORS} from "../../../../data/CONTRIBUTORS";
+import {openLink} from "../../../../utils/helper";
 
 @Component({
   templateUrl: "build/pages/user-center/about-us/profile/index.html"
@@ -8,15 +9,16 @@ import {CONTRIBUTORS} from "../../../../data/CONTRIBUTORS";
 
 export class Profile {
   private person;
-  constructor(public params:NavParams) {
+
+  constructor(public params: NavParams) {
     this.person = CONTRIBUTORS[params.get("num")];
   }
 
   openGitHubUrl(name) {
-    window.open("https://github.com/" + name,  "_system", "location=yes");
+    openLink("https://github.com/" + name);
   }
 
   openBlogUrl(blog) {
-    window.open(blog,  "_system", "location=yes");
+    openLink(blog);
   }
 }
