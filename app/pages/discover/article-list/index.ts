b3ea4080fc4779e2f5d9a0a57ac9a46525b0a81a
Fengda HUANG <h@phodal.com>
2016-08-17 07:51:05 +0800

    revert articles list items

--- app/pages/discover/article-list/index.ts
+++ app/pages/discover/article-list/index.ts
@@ -2,10 +2,10 @@ import {Component} from "@angular/core";
 import {NavController, LoadingController} from "ionic-angular/index";
 import {Http, HTTP_PROVIDERS} from "@angular/http";
 import "rxjs/add/operator/map";
-import {ToolboxDetailPage} from "../toolbox-detail/index";
 import {SERVER_BASE_URL} from "../../../utils/constants";
 import {getSpinnerConfig} from "../../../utils/helper";
 import {ArticleDetailPage} from "../article-detail/index";
+import {reverse} from "lodash";
 
 @Component({
   templateUrl: "build/pages/discover/article-list/index.html",
@@ -29,7 +29,7 @@ export class ArticleListPage {
       .map(res => res.json())
       .subscribe(
         data => {
-          self.articles = data["content"];
+          self.articles = reverse(data["content"]);
           loading.dismiss();
         }
       );
