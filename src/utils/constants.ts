3ef48a45a64596e657994eef64fccbf6accf6846
Fengda HUANG <h@phodal.com>
2016-12-21 22:43:55 +0800

    [migrate] update api to coding

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,11 +1,21 @@
 export const ANIMATION_DURATION = 500;
 
+// export const SERVER_BASE_URL = {
+//   toolbox: "http://toolbox.phodal.com/",
+//   forum: "http://forum.growth.ren/api/discussions",
+//   project: "http://awesome-practise-project.phodal.com/",
+//   articles: "http://articles.phodal.com/",
+//   growthAction: "http://growth-in-action.phodal.com/",
+//   roadmap: "http://awesome-growth.phodal.com/",
+//   ideabook: "http://ideabook.phodal.com/"
+// };
+
 export const SERVER_BASE_URL = {
-  toolbox: "http://toolbox.phodal.com/",
+  toolbox: "https://phodal.coding.me/growth/toolbox/",
   forum: "http://forum.growth.ren/api/discussions",
-  project: "http://awesome-practise-project.phodal.com/",
-  articles: "http://articles.phodal.com/",
-  growthAction: "http://growth-in-action.phodal.com/",
-  roadmap: "http://awesome-growth.phodal.com/",
-  ideabook: "http://ideabook.phodal.com/"
+  project: "https://phodal.coding.me/growth/project/",
+  articles: "https://phodal.coding.me/growth/articles/",
+  growthAction: "https://phodal.coding.me/growth/growth-in-action/",
+  roadmap: "https://phodal.coding.me/growth/awesome/",
+  ideabook: "https://phodal.coding.me/growth/idea/book/"
 };
