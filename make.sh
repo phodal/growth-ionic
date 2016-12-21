6fd26425b60c0633a40ca05aef9bda24ee47fc5b
Fengda HUANG <h@phodal.com>
2016-12-21 14:31:16 +0800

    [migrate] update build script

--- make.sh
+++ make.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-cordova build --release android
+ionic build android --release --prod
 rm ~/repractise/growth/platforms/android/build/outputs/apk/growth.apk
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/phodal.keystore ~/repractise/growth/platforms/android/build/outputs/apk/android-release-unsigned.apk phodal
 ~/android-sdk/build-tools/23.0.1/zipalign -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk ~/repractise/growth/platforms/android/build/outputs/apk/growth.apk
