df9b140b50eeefd51f384dda916e8707c200f9a0
Fengda HUANG <h@phodal.com>
2016-12-23 08:44:57 +0800

    fix page tranition issue

--- src/pages/effect/content-transition.ts
+++ src/pages/effect/content-transition.ts
@@ -1,5 +1,5 @@
 import {ANIMATION_DURATION} from "../../utils/constants";
-import {Transition, ViewController, Animation, PageTransition} from "ionic-angular";
+import {Transition, Animation, PageTransition} from "ionic-angular";
 import { isPresent } from 'ionic-angular/util/util';
 
 export const TRANSITION_IN_KEY:string = "bodyContentEnter";
@@ -52,68 +52,76 @@ export class FadeTransition extends PageTransition {
   }
 }
 
-export class BodyContentInTransition extends Transition {
-  // constructor(parameters: {enteringView: ViewController, leavingView: ViewController, opts: TransitionOptions}) {
-  //   let {enteringView, leavingView, opts} = parameters;
-  //   super(enteringView, leavingView, opts);
-  //
-  //   // DOM READS
-  //   let enteringElement = <HTMLElement> enteringView.pageRef().nativeElement;
-  //   let enteringContent = enteringElement.querySelector(".content-container");
-  //   let enteringAnimation = new Animation(enteringContent);
-  //   enteringAnimation.fromTo("translateY", `${150}px`, `${0}px`);
-  //   enteringAnimation.fromTo("opacity", `0.0`, `1.0`);
-  //
-  //   let exitingAnimation = null;
-  //   if (leavingView.pageRef()) {
-  //     let exitingElement = <HTMLElement> leavingView.pageRef().nativeElement;
-  //     let exitingContent = exitingElement.querySelector(".content-container");
-  //     exitingAnimation = new Animation(exitingContent);
-  //     exitingAnimation.fromTo("translateY", `${0}px`, `${-150}px`);
-  //     exitingAnimation.fromTo("opacity", `1.0`, `0.0`);
-  //   }
-  //
-  //   this.element(enteringView.pageRef()).easing("ease").duration(ANIMATION_DURATION)
-  //     .beforeAddClass("show-page")
-  //     .add(enteringAnimation);
-  //
-  //   if (exitingAnimation) {
-  //     this.add(exitingAnimation);
-  //   }
-  //   if (opts['ev'] && opts['ev'].animation) {
-  //     this.add(opts['ev'].animation);
-  //   }
-  // }
+export class BodyContentInTransition extends PageTransition {
+  init() {
+    super.init();
+
+    const enteringView = this.enteringView;
+    const leavingView = this.leavingView;
+    const opts = this.opts;
+
+    // DOM READS
+    let enteringElement = <HTMLElement> enteringView.pageRef().nativeElement;
+    let enteringContent = enteringElement.querySelector(".content-container");
+    let enteringAnimation = new Animation(enteringContent);
+    enteringAnimation.fromTo("translateY", `${150}px`, `${0}px`);
+    enteringAnimation.fromTo("opacity", `0.0`, `1.0`);
+
+    let exitingAnimation = null;
+    if (leavingView.pageRef()) {
+      let exitingElement = <HTMLElement> leavingView.pageRef().nativeElement;
+      let exitingContent = exitingElement.querySelector(".content-container");
+      exitingAnimation = new Animation(exitingContent);
+      exitingAnimation.fromTo("translateY", `${0}px`, `${-150}px`);
+      exitingAnimation.fromTo("opacity", `1.0`, `0.0`);
+    }
+
+    this.element(enteringView.pageRef()).easing("ease").duration(ANIMATION_DURATION)
+      .beforeAddClass("show-page")
+      .add(enteringAnimation);
+
+    if (exitingAnimation) {
+      this.add(exitingAnimation);
+    }
+    if (opts['ev'] && opts['ev'].animation) {
+      this.add(opts['ev'].animation);
+    }
+  }
 }
-export class BodyContentOutTransition extends Transition {
-  // constructor(enteringView:ViewController, leavingView:ViewController, opts:TransitionOptions) {
-  //   super(enteringView, leavingView, opts);
-  //   // DOM READS
-  //   let enteringElement = <HTMLElement> enteringView.pageRef().nativeElement;
-  //   let enteringContent = enteringElement.querySelector(".content-container");
-  //   let enteringAnimation = new Animation(enteringContent);
-  //   enteringAnimation.fromTo("translateY", `${150}px`, `${0}px`);
-  //   enteringAnimation.fromTo("opacity", `0.0`, `1.0`);
-  //
-  //   let exitingAnimation = null;
-  //   if (leavingView.pageRef()) {
-  //     let exitingElement = <HTMLElement> leavingView.pageRef().nativeElement;
-  //     let exitingContent = exitingElement.querySelector(".content-container");
-  //     exitingAnimation = new Animation(exitingContent);
-  //     exitingAnimation.fromTo("translateY", `${0}px`, `${-150}px`);
-  //     exitingAnimation.fromTo("opacity", `1.0`, `0.0`);
-  //   }
-  //
-  //   this.element(leavingView.pageRef()).easing("ease").duration(ANIMATION_DURATION)
-  //     .beforeAddClass("show-page")
-  //     .add(enteringAnimation);
-  //
-  //   if (exitingAnimation) {
-  //     this.add(exitingAnimation);
-  //   }
-  //
-  //   if (opts['ev'] && opts['ev'].animation) {
-  //     this.add(opts['ev'].animation);
-  //   }
-  // }
+
+export class BodyContentOutTransition extends PageTransition {
+  init() {
+    super.init();
+
+    const enteringView = this.enteringView;
+    const leavingView = this.leavingView;
+    const opts = this.opts;
+
+    let enteringElement = <HTMLElement> enteringView.pageRef().nativeElement;
+    let enteringContent = enteringElement.querySelector(".content-container");
+    let enteringAnimation = new Animation(enteringContent);
+    enteringAnimation.fromTo("translateY", `${150}px`, `${0}px`);
+    enteringAnimation.fromTo("opacity", `0.0`, `1.0`);
+
+    let exitingAnimation = null;
+    if (leavingView.pageRef()) {
+      let exitingElement = <HTMLElement> leavingView.pageRef().nativeElement;
+      let exitingContent = exitingElement.querySelector(".content-container");
+      exitingAnimation = new Animation(exitingContent);
+      exitingAnimation.fromTo("translateY", `${0}px`, `${-150}px`);
+      exitingAnimation.fromTo("opacity", `1.0`, `0.0`);
+    }
+
+    this.element(leavingView.pageRef()).easing("ease").duration(ANIMATION_DURATION)
+      .beforeAddClass("show-page")
+      .add(enteringAnimation);
+
+    if (exitingAnimation) {
+      this.add(exitingAnimation);
+    }
+
+    if (opts['ev'] && opts['ev'].animation) {
+      this.add(opts['ev'].animation);
+    }
+  }
 }
