feacc06e2c6d699c1d906e948e083c9408fa626d
Fengda HUANG <h@phodal.com>
2016-12-23 17:30:44 +0800

    add click event

--- src/pages/skill-tree/skill-tree.ts
+++ src/pages/skill-tree/skill-tree.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController, NavParams} from 'ionic-angular';
 
 @Component({
   selector: 'page-skill-tree',
@@ -7,10 +7,26 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class SkillTreePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
 
   ionViewDidLoad() {
 
   }
 
+  canAddPoints() {
+
+  }
+
+  hasPoints() {
+
+  }
+
+  hasMaxPoints() {
+
+  }
+
+  addPoint(event){
+    console.log(event);
+  }
 }
