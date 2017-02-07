58328f150a4bad8e9374d234729f20a4723382ad
Fengda HUANG <h@phodal.com>
2017-02-07 13:09:33 +0800

    thinking in skill tree professional version

--- src/pages/skill-tree/skill-tree.ts
+++ src/pages/skill-tree/skill-tree.ts
@@ -5,45 +5,49 @@ import {filter} from "lodash";
 import {SkillTreeModal} from "../../components/SkillTreeModal/SkillTreeModal";
 
 @Component({
-  selector: 'page-skill-tree',
-  templateUrl: 'skill-tree.html'
+    selector: 'page-skill-tree',
+    templateUrl: 'skill-tree.html'
 })
 export class SkillTreePage {
 
-  constructor(public navCtrl: NavController, public modalCtrl: ModalController) {
+    constructor(public navCtrl: NavController, public modalCtrl: ModalController) {
 
-  }
+    }
 
-  ionViewDidLoad() {
+    ionViewDidLoad() {
 
-  }
+    }
 
-  canAddPoints() {
+    canAddPoints() {
 
-  }
+    }
 
-  hasPoints() {
+    hasPoints() {
 
-  }
+    }
 
-  hasMaxPoints() {
+    hasMaxPoints() {
 
-  }
+    }
 
-  addPoint(event){
-    event.srcElement.style.fill = '#2d89ef';
-    let pointId = event.srcElement.parentElement.getAttribute('id');
-    this.presentSkillModal(pointId);
-  }
+    getProfessional() {
 
-  removePoint(event){
-    event.srcEvent.srcElement.style.fill = '#ecf0f1';
-  }
+    };
 
-  presentSkillModal(pointId) {
-    let skill = filter(SKILL_TREE, {"id": parseInt(pointId)})[0];
-    let modal = this.modalCtrl.create(SkillTreeModal, {skill: skill});
-    modal.present();
+    addPoint(event) {
+        event.srcElement.style.fill = '#2d89ef';
+        let pointId = event.srcElement.parentElement.getAttribute('id');
+        this.presentSkillModal(pointId);
+    }
 
-  }
+    removePoint(event) {
+        event.srcEvent.srcElement.style.fill = '#ecf0f1';
+    }
+
+    presentSkillModal(pointId) {
+        let skill = filter(SKILL_TREE, {"id": parseInt(pointId)})[0];
+        let modal = this.modalCtrl.create(SkillTreeModal, {skill: skill});
+        modal.present();
+
+    }
 }
