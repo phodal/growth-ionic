77e326d3dfc393b7dd8367f1323c67cd018275b6
Fengda HUANG <h@phodal.com>
2017-02-09 13:11:58 +0800

    fix ion-icon size

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar, Splashscreen } from 'ionic-native';
+import {Component, ViewChild} from '@angular/core';
+import {Platform, ToastController, IonicApp, Nav} from 'ionic-angular';
+import {StatusBar, Splashscreen} from 'ionic-native';
 
-import { TabsPage } from '../pages/tabs/tabs';
+import {TabsPage} from '../pages/tabs/tabs';
 
 
 @Component({
@@ -10,13 +10,52 @@ import { TabsPage } from '../pages/tabs/tabs';
 })
 export class MyApp {
   rootPage = TabsPage;
+  public backButtonPressed: boolean;
+  @ViewChild(Nav) nav: Nav;
 
-  constructor(platform: Platform) {
+  constructor(public platform: Platform, public toastCtrl: ToastController, public ionicApp: IonicApp) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
       Splashscreen.hide();
+      this.registerBackButtonAction();
     });
+  }
+
+  registerBackButtonAction() {
+    this.platform.registerBackButtonAction(() => {
+      //如果想点击返回按钮隐藏toast或loading或Overlay就把下面加上
+      this.ionicApp._toastPortal.getActive() || this.ionicApp._loadingPortal.getActive() || this.ionicApp._overlayPortal.getActive();
+      let activePortal = this.ionicApp._modalPortal.getActive();
+      if (activePortal) {
+        activePortal.dismiss().catch(() => {
+
+        });
+        activePortal.onDidDismiss(() => {
+
+        });
+        return;
+      }
+      let activeVC = this.nav.getActive();
+      let tabs = activeVC.instance.tabs;
+      let activeNav = tabs.getSelected();
+      return activeNav.canGoBack() ? activeNav.pop() : this.showExit()
+    }, 1);
+  }
+
+
+  showExit() {
+    if (this.backButtonPressed) {
+      this.platform.exitApp();
+    } else {
+      this.toastCtrl.create({
+        message: '再按一次退出应用',
+        duration: 2000,
+        position: 'top'
+      }).present();
+      this.backButtonPressed = true;
+      setTimeout(() => this.backButtonPressed = false, 2000);
+    }
   }
 }
