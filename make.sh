6f60cd49a247a3b14712da50f48ebbb098c36281
Fengda HUANG <h@phodal.com>
2017-02-10 07:40:48 +0800

    update build tool version

--- make.sh
+++ make.sh
@@ -2,5 +2,5 @@
 ionic build android --release --prod
 rm ~/repractise/growth/platforms/android/build/outputs/apk/growth.apk
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/phodal.keystore ~/repractise/growth/platforms/android/build/outputs/apk/android-release-unsigned.apk phodal
-~/android-sdk/build-tools/24.0.2/zipalign -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk ~/repractise/growth/platforms/android/build/outputs/apk/growth.apk
+~/android-sdk/build-tools/25.0.2/zipalign -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk ~/repractise/growth/platforms/android/build/outputs/apk/growth.apk
 cp ~/repractise/growth/platforms/android/build/outputs/apk/growth.apk ~/Desktop/growth.apk
