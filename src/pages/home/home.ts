3f99cb4f3f67beefcead41f320e9ac3b6e95d2fc
Fengda HUANG <h@phodal.com>
2017-02-10 14:01:22 +0800

    set version for check update

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -20,32 +20,30 @@ export class HomePage {
   constructor(public navCtrl: NavController,  public analytics:AnalyticsServices, public http: Http,
               public helper: Helper, private alertCtrl: AlertController, public platform: Platform) {
     this.analytics.trackView("Growth 2.0");
+    if(window['cordova'] && this.platform.is('android')){
+      this.updateVersion();
+    }
+  }
+
+  private updateVersion() {
     let self = this;
     this.http.get('http://growth.ren/version.json')
       .map(res => res.json())
       .subscribe(
         data => {
-          if(self.version && self.platform.is('android') && self.helper.versionCompare(data.version, self.version, null) > 0) {
-            self.presentConfirm(data.feature)
-          }
+          AppVersion.getVersionNumber().then(
+            version => {
+              self.version = version;
+              if (self.version && self.helper.versionCompare(data.version, self.version, null) > 0) {
+                self.presentConfirm(data.feature, data.version)
+              }
+            }
+          );
         }
       );
   }
 
-  ionViewWillEnter(){
-    let self = this;
-    if(window['cordova']) {
-      AppVersion.getVersionNumber().then(
-        version => {
-          return self.version = version;
-        }
-      );
-    } else {
-      return self.version = '2.4.0'
-    }
-  }
-
-  presentConfirm(feature) {
+  presentConfirm(feature, version) {
     let alert = this.alertCtrl.create({
       title: '发现新版本',
       message: feature,
@@ -60,6 +58,7 @@ export class HomePage {
         {
           text: '更新',
           handler: () => {
+            this.analytics.trackEvent("update", version);
             this.helper.openLink('market://details?id=ren.growth');
           }
         }
