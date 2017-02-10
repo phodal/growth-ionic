b0cfe0acab1869e75eb482083af31e0094f69503
Fengda HUANG <h@phodal.com>
2017-02-10 13:16:56 +0800

    add update logic

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,20 +1,71 @@
-import {NavController} from 'ionic-angular';
+import {NavController, AlertController, Platform} from 'ionic-angular';
 import {Component} from '@angular/core';
 
 import {Section} from "./section/section";
 import {AnalyticsServices} from "../../services/analytics.services";
+import {AppVersion} from "ionic-native";
+import {Http} from "@angular/http";
+import {Helper} from "../../utils/helper";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html',
-  providers: [AnalyticsServices]
+  providers: [AnalyticsServices, Helper]
 })
 
 export class HomePage {
   private shownGroup = false;
+  private version: any;
 
-  constructor(public navCtrl: NavController,  public analytics:AnalyticsServices) {
+  constructor(public navCtrl: NavController,  public analytics:AnalyticsServices, public http: Http,
+              public helper: Helper, private alertCtrl: AlertController, public platform: Platform) {
     this.analytics.trackView("Growth 2.0");
+    let self = this;
+    this.http.get('http://growth.ren/version.json')
+      .map(res => res.json())
+      .subscribe(
+        data => {
+          if(self.version && self.platform.is('android') && self.helper.versionCompare(data.version, self.version, null) > 0) {
+            self.presentConfirm(data.feature)
+          }
+        }
+      );
+  }
+
+  ionViewWillEnter(){
+    let self = this;
+    if(window['cordova']) {
+      AppVersion.getVersionNumber().then(
+        version => {
+          return self.version = version;
+        }
+      );
+    } else {
+      return self.version = '2.4.0'
+    }
+  }
+
+  presentConfirm(feature) {
+    let alert = this.alertCtrl.create({
+      title: '发现新版本',
+      message: feature,
+      buttons: [
+        {
+          text: '取消',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: '更新',
+          handler: () => {
+            this.helper.openLink('market://details?id=ren.growth');
+          }
+        }
+      ]
+    });
+    alert.present();
   }
 
   openSectionDetailsPage(section) {
