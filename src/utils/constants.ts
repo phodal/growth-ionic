7190bcf2f82ea97deafb25da39718344908b45f5
Phodal HUANG <h@phodal.com>
2017-04-23 22:32:41 +0800

    [T] create iam fe chapters

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -17,5 +17,6 @@ export const SERVER_BASE_URL = {
   articles: "https://phodal.coding.me/growth/articles/",
   growthAction: "https://phodal.coding.me/growth/growth-in-action/",
   roadmap: "https://phodal.coding.me/growth/awesome/",
-  ideabook: "https://phodal.coding.me/growth/ideabook/"
+  ideabook: "https://phodal.coding.me/growth/ideabook/",
+  fe: "https://phodal.coding.me/growth/fe/"
 };
